b3711b0222a03878bcb4b2f30dca583037f8e1f1
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2023-12-13 16:36:40 +0000

    Wed, Dec 13, 2023, 4:36 PM +00:00

--- population/population.c
+++ population/population.c
@@ -5,34 +5,32 @@ int main(void)
 {
     // TODO: Prompt for start size
 
-int start;
-do
-{
-    start = get_int("Start size: ");
-}
-while (start < 9);
+    int start;
+    do
+    {
+        start = get_int("Start size: ");
+    }
+    while (start < 9);
 
     // TODO: Prompt for end size
 
-int end;
-do
-{
-    end = get_int("End size: ");
-}
-while (end < start);
-
+    int end;
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < start);
 
     // TODO: Calculate number of years until we reach threshold
 
-int years = 0;
+    int years = 0;
 
-while (start < end)
-{
-    start = start + (start / 3) - (start / 4),
-    years++;
-}
+    while (start < end)
+    {
+        start = start + (start / 3) - (start / 4), years++;
+    }
 
     // TODO: Print number of years
 
-printf ("Years: %i\n", years);
+    printf("Years: %i\n", years);
 }
