7b4bb5d38b4e47d7456f293bd257a2685176d244
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2023-12-13 15:54:29 +0000

    Wed, Dec 13, 2023, 3:54 PM +00:00

--- population/population.c
+++ population/population.c
@@ -6,15 +6,21 @@ int main(void)
     // TODO: Prompt for start size
 
 int n;
-int e;
 do
 {
     n = get_int("Start size: ");
-    e = get_int("End size: ");
 }
-while (n<=9 && )
+while (n <= 9);
 
     // TODO: Prompt for end size
+
+int e;
+do
+{
+    e = get_int("End size: ");
+}
+while (e <= n);
+
 
     // TODO: Calculate number of years until we reach threshold
 
