05356b1bffa738d8ab2a7717852ee93c3b2a8757
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2023-12-13 20:47:02 +0000

    Wed, Dec 13, 2023, 8:47 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -12,7 +12,10 @@ int main(void)
 
     for (int i = 0; i < height; i++)
     {
-        printf("#");
+        for (int j = 0; j < height; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-    printf("\n");
 }
