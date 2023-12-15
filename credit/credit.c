7416bb21e9ed30518b3490fd65a09643c11842b6
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2023-12-15 19:11:58 +0000

    Fri, Dec 15, 2023, 7:11 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,10 +4,17 @@
 int main(void)
 {
 
-    long cardNumber = get_long("Card number: ")
+    long cardNumber = get_long("Card number: ");
     //AMEX = 34xxxxxxxxxxxxx, 37xxxxxxxxxxxxx
     //Mastercard = 5xxxxxxxxxxxxxxx
     //Visa = 4xxxxxxxxxxxxxxx
 
-    if ()
+    if (cardNumber >= 340000000000000 && cardNumber < 350000000000000)
+    {
+        printf("AMEX\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
 }
