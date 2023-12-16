8f2f0355a806675b86c0690666b8b624b6af52f3
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2023-12-16 18:16:24 +0000

    Sat, Dec 16, 2023, 6:16 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -15,7 +15,7 @@ int main(void)
 
     for (i = 0; i < 16; i++)
     {
-        digit[i] = cardNumber % divisor*(10*(i+1));
+        digit[i] = cardNumber % divisor;
 
         printf("%d\n", digit[i]);
     }
