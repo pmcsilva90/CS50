01e374c3f90618f92aa27e23de6a1212e9b53b8d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2023-12-16 17:56:53 +0000

    Sat, Dec 16, 2023, 5:56 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -13,7 +13,8 @@ int main(void)
     int divisor = 10;
     int digitNum = 1;
 
-    digit[0] = (cardNumber % (divisor^digitNum)) / (divisor^(digitNum-1));
+    digit[0] = (cardNumber % (divisor^digitNum));
+    // (divisor^(digitNum-1));
 
     //(10^(digit[1]))/(10^(digit[1]-1));
 
