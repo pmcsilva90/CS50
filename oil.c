191dfa8d4f6d586e36c526c1696d7020f071cbb7
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2023-12-29 15:26:20 +0000

    Fri, Dec 29, 2023, 3:26 PM +00:00

--- oil.c
+++ oil.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
-/*You are making an app that should remind you when its the time to change the oil in your car.
 
-The oil needs to change if the mileage after the last change is over 10.000, or if the last oil change has been over 12 months ago.
-
-Task:
-
-Take the mileage and months values from input and output "Change Oil", in case the conditions above are satisfied.
-
-In case its not the time for an oil change, output "OK".*/
 
 int main() {
     int mileage;
