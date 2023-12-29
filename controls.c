bc5d0cd9306ce8c776286f32f04a9afb0ce99afa
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2023-12-29 15:03:33 +0000

    Fri, Dec 29, 2023, 3:03 PM +00:00

---
+++ controls.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char dir;
+
+    switch(dir)
+    {
+        case 'w':
+        printf("Up");~
+        break;
+        case a:
+        printf("Left");~
+        break;
+        case d:
+        printf("Right");~
+        break;
+        case s:
+        printf("Down");~
+        break;
+        default w:
+        printf("Wrong");
+        break;
+    }
+
+    return 0;
+}
