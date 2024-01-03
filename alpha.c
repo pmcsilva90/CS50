c0d1bcc9de68fc05a5a65ad359de9b237bfe4eeb
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-03 17:35:56 +0000

    Wed, Jan 3, 2024, 5:35 PM +00:00

--- alpha.c
+++ alpha.c
@@ -6,18 +6,18 @@ int main(void)
 {
     string word = get_string("Word: ");
 
-    int i;
+    int i = 0;
     /*for(i = 0; i < strlen(word); i++)
     {
         word[i] = ("%i", word[i]);
     }*/
-    for(i = 0; i < strlen(word), word[i] > 96; i++)
-    {
+    //for(i = 0; i < strlen(word), word[i] > 96; i++)
+    //{
         if (word[i] > word[i + 1])
         {
         printf("False");
         }
         else
         printf("True");
-    }
+    //}
 }
