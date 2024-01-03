0dbdea6df746e4ac9f4227a29aef92c871b39334
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-03 16:47:12 +0000

    Wed, Jan 3, 2024, 4:47 PM +00:00

--- array.c
+++ array.c
@@ -12,14 +12,6 @@ int main(void)
 
     int i;
     int array[lenght];
-
-    for(i = 0; i < lenght; i++)
-    {
-        array[i] = array[i - 1] * 2;
-        printf("%i\n", array[i - 1]);
-    }
-
-
     //array[0] = 1;
 
     /*for(i = 1; i <= lenght; i++)
