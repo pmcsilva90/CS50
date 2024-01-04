339e3c78aee13c231abbe40128661ca59b820cfb
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-04 19:09:52 +0000

    Thu, Jan 4, 2024, 7:09 PM +00:00

--- caesar.c
+++ caesar.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+
 
 int main(argc, argv[])
 {
-    
+    bool valid_key(argv[1]);
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (isdigit(argv[1][i]))
+    }
+
 
 
     if(argc != 2)
