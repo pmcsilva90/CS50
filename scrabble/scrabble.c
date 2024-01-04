64e166bd153e7dca7241d7f00bda5f2952effd9d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-04 14:51:27 +0000

    Thu, Jan 4, 2024, 2:51 PM +00:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    //Player1 word
+    string word1 = get_string("Player 1: ");
+    //Player2 word
+    string word2 = get_string("Player 2: ");
+
+    int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}
+
+
+
+}
