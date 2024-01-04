079ba1ef5df2747edc4af6eaf735208f88cea092
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-04 12:25:20 +0000

    Thu, Jan 4, 2024, 12:25 PM +00:00

---
+++ array2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int array2[n];
+
+    int n = get_int("Size of array2: ");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%i\n", i);
+    }
+    return 0
+}
