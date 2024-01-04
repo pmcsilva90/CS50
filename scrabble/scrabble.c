ae8f37b21f7454be9356ffe5e73c0f3380c238c3
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-04 15:36:41 +0000

    Thu, Jan 4, 2024, 3:36 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,7 +5,7 @@
 
 int compute_score(string word);
 
-//Letter points
+// Letter points
 int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void)
