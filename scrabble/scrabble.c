7756f429a27b61869f2ae1f4452507992ef57255
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-04 14:54:36 +0000

    Thu, Jan 4, 2024, 2:54 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,15 +3,21 @@
 #include <stdio.h>
 #include <string.h>
 
+int compute_score(string word);
+
 int main(void)
 {
     //Player1 word
     string word1 = get_string("Player 1: ");
+
     //Player2 word
     string word2 = get_string("Player 2: ");
 
-    int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}
+    //Letter points
+    int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+}
 
-
-
+int compute_score(string word)
+{
+    
 }
