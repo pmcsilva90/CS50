ea1b2ac7c0f29b2d704c0f0a8c8d724053956553
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-10 17:17:50 +0000

    Wed, Jan 10, 2024, 5:17 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -165,7 +165,7 @@ bool print_winner(void)
     {
         if (candidates[i].votes > voter_count / 2)
         {
-            printf("%s", candidates[i].name);
+            printf("%s\n", candidates[i].name);
             return true;
         }
     }
@@ -176,6 +176,7 @@ bool print_winner(void)
 int find_min(void)
 {
     // TODO
+    
     return 0;
 }
 
