0e60ee29bebc037e07604c1e958369e38635cd69
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-10 17:11:53 +0000

    Wed, Jan 10, 2024, 5:11 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -161,6 +161,14 @@ void tabulate(void)
 bool print_winner(void)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > voter_count / 2)
+        {
+            printf("%s", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
