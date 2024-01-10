fd5f95d73672ee440ea76a554aaa5fa9dce3805e
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-10 17:25:14 +0000

    Wed, Jan 10, 2024, 5:25 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -176,10 +176,10 @@ bool print_winner(void)
 int find_min(void)
 {
     // TODO
-
+    int min_votes;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == false && candidates[i].votes < candidates[i + 1].votes)
+        if (candidates[i].eliminated == false && candidates[i].votes > min_votes)
         {
             min = candidates[i].votes;
         }
