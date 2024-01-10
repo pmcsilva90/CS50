e524961d078dbc110b1bca5ec7399e00531bc72d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-10 19:14:35 +0000

    Wed, Jan 10, 2024, 7:14 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -191,12 +191,12 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
-    int candidates_tie = 0;
+    int candidates_tied = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated == false && candidates[i].votes == min)
         {
-            candidates_tie++;
+            candidates_tied++;
         }
     }
     /*int candidates_left = 0;
@@ -209,7 +209,7 @@ bool is_tie(int min)
         }
     }*/
 
-    if (candidates_tie > 1)
+    if (candidates_tied > 1)
     {
         return true;
     }
