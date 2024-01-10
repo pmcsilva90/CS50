cc6b9b99ec9adc55a7f66e8a3159ce8e1691fea8
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-10 16:45:52 +0000

    Wed, Jan 10, 2024, 4:45 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -147,9 +147,9 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (candidates[preferences[i][j].eliminated = false])
+            if (candidates[preferences[i][j]].eliminated = false)
             {
-                candidates[preferences[i][j].votes]++;
+                candidates[preferences[i][j]].votes]++;
                 break;
             }
         }
