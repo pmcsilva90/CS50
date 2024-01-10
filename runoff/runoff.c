7d7541b94fd738ae33e348694a2c077d8f417732
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-10 18:06:40 +0000

    Wed, Jan 10, 2024, 6:06 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -133,8 +133,8 @@ bool vote(int voter, int rank, string name)
         if (strcmp(name, candidates[i].name) == 0)
         {
             preferences[voter][rank] = i;
+            return true;
         }
-        return true;
     }
     return false;
 }
