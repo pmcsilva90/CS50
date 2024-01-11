b823cf4b1b91d1d42eda4db7f30a5b8c84d0f8b5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-11 18:44:01 +0000

    Thu, Jan 11, 2024, 6:44 PM +00:00

--- sortalgorithms.c
+++ sortalgorithms.c
@@ -2,13 +2,18 @@
 #include <cs50.h>
 #include <strings.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
+    array[];
+    lenght;
+
+
     if (argc == 1)
     {
         get_string ("Sorting Algorithm: ")
     }
-    else if (strcasecmp())
+    else if (strcasecmp(argv[1], "selection"))
 }
 
