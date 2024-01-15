f153b056715fef2c479c9d8819a3a1b681b79f48
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-15 20:37:17 +0000

    Mon, Jan 15, 2024, 8:37 PM +00:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -38,7 +38,7 @@ int convert(string input)
     return result;
     */
 
-    int n = strlen(input) - 1;
+    int n = strlen(*input) - 1;
     int result = 0;
 
     // Base case: when the string is empty
@@ -48,8 +48,8 @@ int convert(string input)
     }
 
     // Convert the last character to an integer
-    result = input[n] - '0';
+    result = *input[n] - '0';
 
     // Recursively convert the substring without the last character
-    return result + 10 * convert(input);
+    return result + 10 * convert(*input);
 }
