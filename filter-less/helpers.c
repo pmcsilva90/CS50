f29384d2472876f87c165ee9b27e4378622250b6
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-17 18:01:08 +0000

    Wed, Jan 17, 2024, 6:01 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,11 +3,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    new_r = 0;
+    new_g = 0;
     for (int h = 0; h < height; h++)
     {
         for (int w = 0; w < width; w++)
         {
-            image[h][w]
+             = image[h][w]
         }
     }
     return;
