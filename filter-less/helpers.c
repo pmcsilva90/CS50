fa7de5cedbe930c321824c2dae1ef1a8177b21f7
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-17 20:19:28 +0000

    Wed, Jan 17, 2024, 8:19 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -9,7 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int w = 0; w < width; w++)
         {
-            avg = round((image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed) / 3);
+            avg = round((image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed) / 3.0);
             image[h][w].rgbtBlue = avg;
             image[h][w].rgbtGreen = avg;
             image[h][w].rgbtRed = avg;
