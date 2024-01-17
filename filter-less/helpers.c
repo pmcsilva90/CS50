22860182cfbc0ac1bda807393a90e403962be744
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-17 19:12:54 +0000

    Wed, Jan 17, 2024, 7:12 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -92,7 +92,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-    
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            
+        }
+    }
 
     return;
 }
