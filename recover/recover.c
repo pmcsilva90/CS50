efe8d7448d4a7b8923b90f5a0f304a47ff2b2e7e
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-18 17:25:46 +0000

    Thu, Jan 18, 2024, 5:25 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,10 +24,19 @@ int main(int argc, char *argv[])
     // Create a buffer for a block of data
     uint8_t buffer[512];
     uint8_t BYTE1, BYTE2, BYTE3, BYTE4;
+    
 
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
         // Create JPEGs from the data
+        // Open output file
+        FILE *outptr = fopen(outfile, "w");
+        if (outptr == NULL)
+        {
+            fclose(inptr);
+            printf("Could not create %s.\n", outfile);
+            return 1;
+    }
     }
 }
