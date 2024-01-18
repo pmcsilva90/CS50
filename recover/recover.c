10ff4a6b1a621e5c5d370e448933470a3eef78de
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-18 17:14:56 +0000

    Thu, Jan 18, 2024, 5:14 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
 
     // Create a buffer for a block of data
     uint8_t buffer[512];
+    uint8_t BYTE1, BYTE2, BYTE3, BYTE4;
 
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
