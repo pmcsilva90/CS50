98ed2f6a536f793ad3675d274126f7671b1acf1c
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-25 17:12:01 +0000

    Thu, Jan 25, 2024, 5:12 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -39,11 +39,12 @@ int main(int argc, char *argv[])
         // Create JPEGs from the data
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[4] == somethingggg!!!!)
         {
+            if (counter != 0)
 
 
             while (fread(buffer, 1, 512, card) == 512)
             {
-                filename = sprintf("%3i", counter, filename)
+                filename = sprintf("%3i", counter, filename);
                 FILE *output = fopen("###.jpeg", "w");
                 freadfwrite(buffer, 1, 512, output);
                 fclose(output);
