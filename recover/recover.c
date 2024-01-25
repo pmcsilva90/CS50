c6bc9e9d6b144d168895db96390ea31dbba1b3bb
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-25 16:41:49 +0000

    Thu, Jan 25, 2024, 4:41 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,6 +10,29 @@ int main(int argc, char *argv[])
     // Put another way, the fourth byte’s first four bits are 1110.
     // FAT file system “block size” is 512 bytes (B)
     // 50 JPEGs total
-    
+
+    // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    // Open the memory card
+    FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
+    {
+        
+    }
+
+    // Create a buffer for a block of data
+    uint8_t buffer[512];
+
+    // While there's still data left to read from the memory card
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        // Create JPEGs from the data
+
+    }
 
 }
