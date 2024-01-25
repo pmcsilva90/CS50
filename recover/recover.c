81d5cbff67e4b6245b4ba6a7c37855c3577cf24d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-25 16:55:03 +0000

    Thu, Jan 25, 2024, 4:55 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,14 +28,21 @@ int main(int argc, char *argv[])
 
     // Create a buffer for a block of data
     uint8_t buffer[512];
+    char filename[8];
+    int counter = 0;
+
+
 
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
         // Create JPEGs from the data
-        
+        if (buffer[0] == 0xff || buffer[1] == 0xd8 || buffer[2] == 0xff || buffer[4] == somethingggg!!!!)
+        {
+            
+            fopen("###.jpeg")
+        }
 
-        fopen()
     }
 
 }
