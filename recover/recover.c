8559783ff99b4fd31c2916dd823a304ec5db01d9
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-25 16:35:16 +0000

    Thu, Jan 25, 2024, 4:35 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
     // 4th byte is either 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, or 0xef
     // Put another way, the fourth byte’s first four bits are 1110.
     // FAT file system “block size” is 512 bytes (B)
+    // 50 JPEGs total
     
 
 }
