5ee7409f6d888508ad8dce8aa56deeaa334ac4cc
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-01-25 18:56:18 +0000

    Thu, Jan 25, 2024, 6:56 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             // If not first JPEG found, close previous one
-            if (counter > 0)
+            if (counter != 0)
             {
                 fclose(output);
             }
