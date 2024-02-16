085a90231bd982100706156730e533f102562740
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-16 17:11:26 +0000

    Fri, Feb 16, 2024, 5:11 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,9 +5,9 @@ int main(void)
 {
     long card = get_long("Number: ");
 
-    // AMEX: 15 digit, 34 or 37
-    // Mastercard: 16 digit, 51, 52, 53, 54, 55
-    // Visa: 13 and 16 digit, 4
+    // AMEX: 15 digit, starting 34 or 37
+    // Mastercard: 16 digit, starting 51, 52, 53, 54 or 55
+    // Visa: 13 and 16 digit, starting 4
 
     long digits = card;
     int checksum = 0;
@@ -16,26 +16,26 @@ int main(void)
     {
         checksum += digits % 10;
         digits /= 10;
-        if(digits % 10 * 2 > 9)
+        if (digits % 10 * 2 > 9)
         {
             checksum += digits % 10 * 2 % 10 + digits % 10 * 2 / 10;
         }
         else
-        checksum += digits % 10 * 2;
+            checksum += digits % 10 * 2;
         digits /= 10;
     }
 
-    if(checksum % 10 == 0)
+    if (checksum % 10 == 0)
     {
-        if(card / 10000000000000 == 34 || card / 10000000000000 == 37)
+        if (card / 10000000000000 == 34 || card / 10000000000000 == 37)
         {
             printf("AMEX\n");
         }
-        else if(card / 100000000000000 >= 51 && card / 100000000000000 <= 55)
+        else if (card / 100000000000000 >= 51 && card / 100000000000000 <= 55)
         {
             printf("MASTERCARD\n");
         }
-        else if(card / 1000000000000 == 4 || card / 1000000000000000 == 4)
+        else if (card / 1000000000000 == 4 || card / 1000000000000000 == 4)
         {
             printf("VISA\n");
         }
@@ -45,6 +45,5 @@ int main(void)
         }
     }
     else
-    printf("INVALID\n");
+        printf("INVALID\n");
 }
-
