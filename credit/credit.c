bda5ddf972c3f2d53a581fa866f71ebe0e269c35
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-16 15:01:18 +0000

    Fri, Feb 16, 2024, 3:01 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,5 +5,12 @@ int main(void)
 {
     long card = get_long("Number: ")
 
-    if()
+    // AMEX: 15 digit, 34 or 37
+    // Mastercard: 16 digit, 51, 52, 53, 54, 55
+    // Visa: 13 and 16 digit, 4
+
+    if(card / 10000000000000 == 34 || card / 10000000000000 == 37)
+    {
+        printf()
+    }
 }
