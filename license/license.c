4f70129fbc647384ebdceead2a6f6adebacdf7c7
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-17 19:59:33 +0000

    Sat, Feb 17, 2024, 7:59 PM +00:00

--- license/license.c
+++ license/license.c
@@ -1,1 +1,47 @@
-license/license.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    // Check for command line args
+    if (argc != 2)
+    {
+        printf("Usage: ./read infile\n");
+        return 1;
+    }
+
+    // Create buffer to read into
+    char buffer[7];
+
+    // Create array to store plate numbers
+    char *plates[8];
+
+    FILE *infile = fopen(argv[1], "r");
+
+    int idx = 0;
+
+    while (fread(buffer, 1, 7, infile) == 7)
+    {
+        // Replace '\n' with '\0'
+        buffer[6] = '\0';
+
+        // Allocate memory for each plate number and copy contents
+        plates[idx] = strdup(buffer);
+        idx++;
+    }
+
+    fclose(infile);
+
+    // Print the plate numbers
+    for (int i = 0; i < idx; i++)
+    {
+        printf("%s\n", plates[i]);
+
+        // Free allocated memory
+        free(plates[i]);
+    }
+
+    return 0;
+}
