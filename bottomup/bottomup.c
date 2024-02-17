c930adc1c932fd98e20604aca7d821ba7af5380a
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-17 18:18:38 +0000

    Sat, Feb 17, 2024, 6:18 PM +00:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
         return 4;
     }
 
-    
+    bi.biHeight = abs(bi.biHeight);
 
     // Write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
