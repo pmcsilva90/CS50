f1ec08b10d590edc76911b63bfbefab5ab5fa630
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-17 19:50:08 +0000

    Sat, Feb 17, 2024, 7:50 PM +00:00

--- license/license.c
+++ license/license.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
         buffer[6] = '\0';
 
         // Save plate number in array
-        plates[idx] = strdup(buffer);
+        plates[idx] = strdupa(buffer);
         idx++;
     }
 
