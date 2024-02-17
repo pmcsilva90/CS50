a51aa5e19e063aaba0ab339d34064f9cd3a85aa3
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-17 19:42:47 +0000

    Sat, Feb 17, 2024, 7:42 PM +00:00

--- license/license.c
+++ license/license.c
@@ -26,9 +26,8 @@ int main(int argc, char *argv[])
         buffer[6] = '\0';
 
         // Save plate number in array
-        plates[idx] = *buffer;
+        plates[idx] = buffer;
         idx++;
-        //printf("%s\n", buffer);
     }
 
     for (int i = 0; i < 8; i++)
