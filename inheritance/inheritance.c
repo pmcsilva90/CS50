3073f0e4df528bf0b426a1d9e540f7db61741ecc
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-25 16:55:00 +0000

    Sun, Feb 25, 2024, 4:55 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,6 +40,11 @@ person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
     person *new_person = malloc(sizeof(person));
+    if (new_person == NULL)
+    {
+        free_family(p)
+        return 1;
+    }
 
     // If there are still generations left to create
     if (generations > 1)
