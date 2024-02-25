4c28403ff615139e38837918ce28969a22e23b81
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-25 17:11:29 +0000

    Sun, Feb 25, 2024, 5:11 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -58,8 +58,8 @@ person *create_family(int generations)
         new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person->alleles[0] = parent0.alleles[]
-        new_person->alleles[1] =
+        new_person->alleles[0] = parent0.alleles[rand() % 2]
+        new_person->alleles[1] = parent1.alleles[rand() % 2]
     }
 
     // If there are no generations left to create
