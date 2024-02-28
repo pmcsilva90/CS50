650f67a5c17032e97b33a1aa94bb74366e829073
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-28 17:01:21 +0000

    Wed, Feb 28, 2024, 5:01 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -115,13 +115,14 @@ bool unload(void)
     for (int i = 0; i < N; i++)
     {
         node *cursor = table[i];
-        node *tmp = table[i];
+        node *tmp = cursor;
         while (cursor->next =! NULL)
         {
-            
+            cursor = cursor->next;
+            free(tmp);
+            tmp = cursor;
         }
     }
 
-    free(new_node);
     return false;
 }
