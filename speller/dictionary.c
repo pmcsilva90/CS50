cf5f6f65bda86164a41b634fc8cf2291099a0ff5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-02-28 16:37:05 +0000

    Wed, Feb 28, 2024, 4:37 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -94,7 +94,7 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return int *word_count;
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
