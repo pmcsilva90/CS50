66acc40937880b1b31fbf63dbf3ec40406afeee0
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-06 19:44:58 +0000

    Wed, Mar 6, 2024, 7:44 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -18,25 +18,15 @@ while digits > 0:
         checksum += digits % 10 * 2
     digits //= 10
 
-if (checksum % 10 == 0)
-{
-    if (card / 10000000000000 == 34 || card / 10000000000000 == 37)
-    {
-        printf("AMEX\n");
-    }
-    else if (card / 100000000000000 >= 51 && card / 100000000000000 <= 55)
-    {
-        printf("MASTERCARD\n");
-    }
-    else if (card / 1000000000000 == 4 || card / 1000000000000000 == 4)
-    {
-        printf("VISA\n");
-    }
-    else
-    {
-        printf("INVALID\n");
-    }
-}
-else
-    printf("INVALID\n");
+if checksum % 10 == 0:
+    if card // 10000000000000 == 34 or card // 10000000000000 == 37:
+        print("AMEX")
+    elif card // 100000000000000 >= 51 and card // 100000000000000 <= 55:
+        print("MASTERCARD")
+    elif card // 1000000000000 == 4 or card // 1000000000000000 == 4:
+        print("VISA")
+    else:
+        print("INVALID")
+else:
+    print("INVALID")
 
