456beb71de646b41321b560f8e5a6cc39da70ba0
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-06 10:22:08 +0000

    Wed, Mar 6, 2024, 10:22 AM +00:00

--- CS50Python/faces/faces.py
+++ CS50Python/faces/faces.py
@@ -1,5 +1,13 @@
-def main()
+def main():
+    string = input()
+
+    convert(string)
+
+    print(string)
 
 def convert(emoticon):
-    
+    emoticon.replace(":\)","🙂")
+    emoticon.replace(":\()","🙁")
+    emoticon.replace(":|)","😐")
 
+main()
