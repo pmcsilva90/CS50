dd256f3c832928fcd539f2a6cac9ee293f251a3a
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-06 18:55:55 +0000

    Wed, Mar 6, 2024, 6:55 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,4 +1,10 @@
 from cs50 import get_int
 
+height
 
-height = get_int("Height: ")
+while height < 0 and height > 8:
+    height = get_int("Height: ")
+
+for i in range(height)
+    for j in range(i + 1)
+        print("#", end="")
