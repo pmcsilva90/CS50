628ff4f722f3af62246d823d4239cc10b1424cc4
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 18:00:51 +0000

    Thu, Mar 7, 2024, 6:00 PM +00:00

--- CS50Python/pset2/twttr/twttr.py
+++ CS50Python/pset2/twttr/twttr.py
@@ -1,3 +1,10 @@
 input = input("Input: ")
 
+print("Output: ", end="")
 
+for c in input:
+    if c in ['a', 'e', 'i', 'o', 'u']:
+        print("", end="")
+    else:
+        print(c, end="")
+print()
