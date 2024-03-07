e548dea4bd1dffb68f652f8ed466905206844340
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 12:55:15 +0000

    Thu, Mar 7, 2024, 12:55 PM +00:00

--- sentimental-mario-more/mario2.py
+++ sentimental-mario-more/mario2.py
@@ -1,9 +1,9 @@
 from cs50 import get_int
 
-height = get_int("Height: ")
+#height = get_int("Height: ")
 
-while height < 1 or height > 8:
-    height = get_int("Height: ")
+#while height < 1 or height > 8:
+#    height = get_int("Height: ")
 
 #for i in range(height):
 #    for _ in range(height - i - 1):
@@ -14,6 +14,14 @@ while height < 1 or height > 8:
 #    for _ in range(i + 1):
 #        print("#", end="")
 #    print()
+
+while True:
+    height = get_int("Height: ")
+    if height < 1 or height > 8:
+        continue
+    else:
+        break
+
 
 for i in range(height):
     print(" " * (height - i - 1), end="")
