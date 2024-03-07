ae448dfda73ee3fc8fda8950692e8358777d45ef
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 17:42:23 +0000

    Thu, Mar 7, 2024, 5:42 PM +00:00

--- CS50Python/pset2/coke/coke.py
+++ CS50Python/pset2/coke/coke.py
@@ -1,12 +1,12 @@
 due = 50
 
 while True:
-    print(f"Amount due: {due}")
-    coin = int(input("Insert coin: "))
+    print(f"Amount Due: {due}")
+    coin = int(input("Insert Coin: "))
     if coin == 5 or coin == 10 or coin == 25:
         due -= coin
         if due <= 0:
-            print(f"Change owed: {due * -1}")
+            print(f"Change Owed: {due * -1}")
             break
     else:
         continue
