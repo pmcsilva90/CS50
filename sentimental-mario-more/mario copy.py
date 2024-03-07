88c0fcbb3d837d4a5f330e5fe144d1ad5c1d68e3
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 12:42:10 +0000

    Thu, Mar 7, 2024, 12:42 PM +00:00

---
+++ sentimental-mario-more/mario copy.py
@@ -0,0 +1,16 @@
+from cs50 import get_int
+
+height = get_int("Height: ")
+
+while height < 1 or height > 8:
+    height = get_int("Height: ")
+
+for i in range(height):
+    for _ in range(height - i - 1):
+        print(" ", end="")
+    for _ in range(i + 1):
+        print("#", end="")
+    print("  ", end="")
+    for _ in range(i + 1):
+        print("#", end="")
+    print()
