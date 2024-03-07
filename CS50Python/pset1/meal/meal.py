f4a9a652a28e701353d90c0208e546cc563d2c19
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 12:10:05 +0000

    Thu, Mar 7, 2024, 12:10 PM +00:00

--- CS50Python/pset1/meal/meal.py
+++ CS50Python/pset1/meal/meal.py
@@ -1,14 +1,20 @@
 def main():
     time = input("What time is it? ")
+    convert(time)
 
-    
-
+    if 7 <= time <= 8:
+        print("breakfast time")
+    elif 12 <= time <= 13:
+        print("lunch time")
+    elif 18 <= time <= 19:
+        print("dinner time")
 
 
 def convert(time):
     hour, minute = time.split(":")
+    hour = int(hour)
+    minute = int(minute)
     return hour + (minute / 60)
-
 
 
 if __name__ == "__main__":
