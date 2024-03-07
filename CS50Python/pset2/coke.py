332c59671c61496208941482355bcf08bb2cb4d5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 17:32:06 +0000

    Thu, Mar 7, 2024, 5:32 PM +00:00

--- CS50Python/pset2/coke.py
+++ CS50Python/pset2/coke.py
@@ -1,8 +1,10 @@
 due = 50
 
-print("Amount due: " + due)
-
 while True:
+    print("Amount due: " + due)
     coin = int(input("Insert coin: "))
+    if coin == 5 or coin == 10 or coin == 25:
+        due -= coin
+        
 
 
