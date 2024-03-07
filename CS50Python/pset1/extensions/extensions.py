1f270d7f0b7bbf3800159ed390fd3435589b6fae
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 11:19:17 +0000

    Thu, Mar 7, 2024, 11:19 AM +00:00

--- CS50Python/pset1/extensions/extensions.py
+++ CS50Python/pset1/extensions/extensions.py
@@ -1,3 +1,23 @@
 filename = input("File name: ")
 
-match f
+filename = filename.lower().strip()
+
+match filename:
+    case filename.endswith(".gif"):
+        print("image/gif")
+    case filename.endswith(".jpg" | ".jpeg"):
+        print("image/jpeg")
+    case filename.endswith(".png"):
+        print("image/png")
+    case filename.endswith(".pdf"):
+        print("application/pdf")
+    case filename.endswith(".gif"):
+        print("image/gif")
+    case filename.endswith(".gif"):
+        print("image/gif")
+    case filename.endswith(".gif"):
+        print("image/gif")
+    case filename.endswith(".gif"):
+        print("image/gif")
+    case filename.endswith(".gif"):
+        print("image/gif")
