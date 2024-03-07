e6d68f064a3041806d65339161ec9c4d9465197e
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 16:49:21 +0000

    Thu, Mar 7, 2024, 4:49 PM +00:00

--- CS50Python/pset2/camel/camel.py
+++ CS50Python/pset2/camel/camel.py
@@ -1,6 +1,12 @@
 var = input("camelCase: ")
 
+char_counter = 0
+
 for c in var:
+    char_counter += 1
     if c.isupper():
         c = c.lower()
-        print()
+        for _ in range(char_counter):
+            print(c + "_", end="")
+    print(c )
+
