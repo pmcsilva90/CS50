c905f2e607fc226e39dc6a27900f55790fde1e92
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 10:43:39 +0000

    Thu, Mar 7, 2024, 10:43 AM +00:00

--- CS50Python/pset1/deep/deep.py
+++ CS50Python/pset1/deep/deep.py
@@ -1,6 +1,7 @@
 answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
 
 answer = answer.lower()
+answer = answer.strip()
 
 if answer == "42" or answer == "forty two" or answer == "forty-two":
     print("Yes")
