db1533194c9a2fcebb09a9564a35652f66fbfe47
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 12:46:52 +0000

    Thu, Mar 7, 2024, 12:46 PM +00:00

--- sentimental-mario-more/mario copy.py
+++ sentimental-mario-more/mario copy.py
@@ -5,12 +5,19 @@ height = get_int("Height: ")
 while height < 1 or height > 8:
     height = get_int("Height: ")
 
+#for i in range(height):
+#    for _ in range(height - i - 1):
+#        print(" ", end="")
+#    for _ in range(i + 1):
+#        print("#", end="")
+#    print("  ", end="")
+#    for _ in range(i + 1):
+#        print("#", end="")
+#    print()
+
 for i in range(height):
-    for _ in range(height - i - 1):
-        print(" ", end="")
-    for _ in range(i + 1):
-        print("#", end="")
+    print(" " * (height - i - 1), end="")
+    print("#" * (i + 1), end="")
     print("  ", end="")
-    for _ in range(i + 1):
-        print("#", end="")
+    print("#" * (i + 1), end="")
     print()
