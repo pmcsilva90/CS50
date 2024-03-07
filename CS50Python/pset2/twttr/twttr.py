74b3bbb33380154d38b74787d17b3adb72812874
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-07 18:04:25 +0000

    Thu, Mar 7, 2024, 6:04 PM +00:00

--- CS50Python/pset2/twttr/twttr.py
+++ CS50Python/pset2/twttr/twttr.py
@@ -3,7 +3,7 @@ input = input("Input: ")
 print("Output: ", end="")
 
 for c in input:
-    if c in ['a', 'e', 'i', 'o', 'u']:
+    if c.lower in ['a', 'e', 'i', 'o', 'u']:
         print("", end="")
     else:
         print(c, end="")
