51f3b01d3ac40cda4c9097cac591c5f70ca29483
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-08 14:54:31 +0000

    Fri, Mar 8, 2024, 2:54 PM +00:00

---
+++ CS50Python/nutrition/nutrition.py
@@ -0,0 +1,31 @@
+fruits = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80,
+    }
+
+def main():
+    fruit = input("Item: ")
+
+    fruit = fruit.lower()
+
+    if fruit in fruits:
+        print("Calories: " + fruits[1])
+
+main()
+
