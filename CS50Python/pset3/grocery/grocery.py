e4c3741f4ed1664ee9941f9364aea590f7e73308
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-09 13:38:32 +0000

    Sat, Mar 9, 2024, 1:38 PM +00:00

--- CS50Python/pset3/grocery/grocery.py
+++ CS50Python/pset3/grocery/grocery.py
@@ -4,8 +4,10 @@ while True:
     try:
         item = input()
         item = item.upper()
-        grocery[item] += 1
+        grocery[item] = 1
+        if grocery[item] > 0:
+            grocery[item] += 1
     except EOFError:
         for item in grocery:
-            print(grocery[item], item, sep=",")
+            print(grocery[item], item, sep=", ")
         break
