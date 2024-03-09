2208a6a302bf8c3f727ee3b64cb72bb58935ba3c
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-09 16:04:53 +0000

    Sat, Mar 9, 2024, 4:04 PM +00:00

--- CS50Python/pset3/outdated/outdated.py
+++ CS50Python/pset3/outdated/outdated.py
@@ -18,18 +18,22 @@ while True:
         date = input("Date: ")
         date = date.strip()
         month, day, year = date.split("/")
-        if 0 < month <= 12 and 0 < day <= 31:
+        if 0 < int(month) <= 12 and 0 < int(day) <= 31:
             break
     except ValueError:
         try:
             month, day, year = date.split(" ")
             month = month.capitalize()
-            if month in months and 0 < day <= 31:
+            if month in months and 0 < int(day) <= 31:
                 month = months.index(month) + 1
                 break
             else:
                 pass
         except ValueError:
             pass
+
+month = int(month)
+day = int(day)
+year = int(year)
 
 print(year, month, day, sep="-")
