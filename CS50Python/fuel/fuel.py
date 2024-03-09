16ff03daa91089dca8c958696172ec7f9c7f2a62
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-09 12:12:35 +0000

    Sat, Mar 9, 2024, 12:12 PM +00:00

--- CS50Python/fuel/fuel.py
+++ CS50Python/fuel/fuel.py
@@ -1,9 +1,9 @@
-gauge = input("Fraction: ")
-
-x, y = gauge.split(sep="/")
-
 while True:
     try:
+        gauge = input("Fraction: ")
+
+        x, y = gauge.split(sep="/")
+
         x = int(x)
         y = int(y)
         break
