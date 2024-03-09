4e309a0871ede1b923e7866f396e3336f2280ee4
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-09 15:29:02 +0000

    Sat, Mar 9, 2024, 3:29 PM +00:00

---
+++ CS50Python/pset3/outdated/outdated.py
@@ -0,0 +1,18 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+date = input("Date: ")
+
+
