859fcf78ac35c5df21a0fd2faa539c1531103833
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-10 18:38:32 +0000

    Sun, Mar 10, 2024, 6:38 PM +00:00

--- CS50Python/pset4/bitcoin/bitcoin.py
+++ CS50Python/pset4/bitcoin/bitcoin.py
@@ -3,8 +3,10 @@ import requests
 import sys
 
 try:
-    n = (int)sys.argv[1]
+    n = int(sys.argv[1])
 except ValueError:
     sys.exit("Not a valid value")
+except IndexError:
+    sys.exit("Too few arguments")
 
 print(n)
