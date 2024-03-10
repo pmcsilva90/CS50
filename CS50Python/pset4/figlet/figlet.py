080035b1aa2af0ec900a94cac08a4e4bd09a5c9d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-10 12:34:43 +0000

    Sun, Mar 10, 2024, 12:34 PM +00:00

--- CS50Python/pset4/figlet/figlet.py
+++ CS50Python/pset4/figlet/figlet.py
@@ -8,10 +8,10 @@ from pyfiglet import Figlet
 #figlet.setFont(font=f)
 
 
-#str = input("Input: ")
+str = input("Input: ")
 
 #print(figlet.renderText(str))
 
 f = Figlet(font='slant')
 
-print(f.renderText('text to render'))
+print(f.renderText(str))
