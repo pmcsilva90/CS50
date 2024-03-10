641533a819d713bb8774c03a42c866b513e8dc23
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-10 14:13:53 +0000

    Sun, Mar 10, 2024, 2:13 PM +00:00

--- CS50Python/pset4/adieu/adieu.py
+++ CS50Python/pset4/adieu/adieu.py
@@ -1,8 +1,15 @@
 import inflect
 
+p = inflect.engine()
+
 names = []
 
 while True:
     try:
-        name
+        name = input("Name: ")
+        names.append(name)
+    except EOFError:
+        break
+
+print(p.join(names), final_sep="")
 
