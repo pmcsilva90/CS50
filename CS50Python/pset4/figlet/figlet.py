87f1f083cdad1df012b626b1f42864bf8cb2c50d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-10 12:51:09 +0000

    Sun, Mar 10, 2024, 12:51 PM +00:00

--- CS50Python/pset4/figlet/figlet.py
+++ CS50Python/pset4/figlet/figlet.py
@@ -6,8 +6,7 @@ fonts[] = figlet.getFonts()
 
 if len(sys.argv) == 0:
     # random font
-    if sys.argv[2] in fonts:
-        f = Figlet(font=random.choice(fonts))
+    f = Figlet(font=random.choice(fonts))
 
 elif len(sys.argv) == 2:
     # argv[1] is "-f" or "--font", argv[2] is fontName
