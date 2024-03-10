35aaf3de1a366ab79143cfa5e4dced2c215d4732
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-10 12:31:48 +0000

    Sun, Mar 10, 2024, 12:31 PM +00:00

--- CS50Python/pset4/figlet/figlet.py
+++ CS50Python/pset4/figlet/figlet.py
@@ -2,12 +2,15 @@ import sys
 import random
 from pyfiglet import Figlet
 
-figlet = Figlet()
+#figlet = Figlet()
 
-fonts = figlet.getFonts()
-figlet.setFont(font=f)
+#fonts = figlet.getFonts()
+#figlet.setFont(font=f)
 
 
-str = input("Input: ")
+#str = input("Input: ")
 
-print(figlet.renderText(str))
+#print(figlet.renderText(str))
+
+f = Figlet(font='slant')
+print f.renderText('text to render')
