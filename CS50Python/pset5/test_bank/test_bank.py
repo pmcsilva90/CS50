093fec39974c900782cb664a0eb8ba50e8db72fd
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-11 19:55:54 +0000

    Mon, Mar 11, 2024, 7:55 PM +00:00

--- CS50Python/pset5/test_bank/test_bank.py
+++ CS50Python/pset5/test_bank/test_bank.py
@@ -1,10 +1,33 @@
 from bank import value
 
-def test_lower:
-    c
+def test_hello:
+    assert value("hello") == 0
+    assert value("HELLO") == 0
+    assert value("HeLlO") == 0
+    assert value("hElLo") == 0
+    assert value("Hello, friend!") == 0
+    assert value("Hello!!1!1") == 0
 
-def test_upper:
 
-def test_digit:
+def test_h:
+    assert value("hey") == 20
+    assert value("HEY") == 20
+    assert value("Hi") == 20
+    assert value("hI") == 20
+    assert value("Hi, friend!") == 20
+    assert value("Heeeyyy!!1!11|") == 20
+    assert value("Hola") == 20
+    assert value("HOLA, qUe Tal???") == 20
 
-def test_non_alnum:
+
+
+def test_else:
+    assert value("Good morning!") == 100
+    assert value("") == 100
+    assert value("") == 100
+    assert value("") == 100
+    assert value("") == 100
+    assert value("") == 100
+    assert value("") == 100
+    assert value("") == 100
+
