ceaea47e1814cc3ff091d791b314c168cd439ab2
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-11 19:44:15 +0000

    Mon, Mar 11, 2024, 7:44 PM +00:00

--- CS50Python/pset5/test_bank/bank.py
+++ CS50Python/pset5/test_bank/bank.py
@@ -1,10 +1,18 @@
 def main():
     greeting = input("Greeting: ")
-    
+    value = value(greeting)
+    print(f"${value}")
 
 
 def value(greeting):
-    return int
+    greeting = greeting.strip().lower()
+
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("h"):
+        return 20
+    else:
+        return 100
 
 
 if __name__ == "__main__":
