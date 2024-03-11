bb24082df7779b3b8e0dfedbe4b7ede33858245f
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-11 19:03:59 +0000

    Mon, Mar 11, 2024, 7:03 PM +00:00

--- CS50Python/pset5/test_twttr/twttr.py
+++ CS50Python/pset5/test_twttr/twttr.py
@@ -6,7 +6,11 @@ def main():
 
 def shorten(word):
     vowels = "aeoiu"
-    
+    shortened_word = []
+    for char in word:
+        if char.lower() not in vowels:
+            shortened_word.append(char)
+    return shortened_word
 
 
 if __name__ == "__main__":
