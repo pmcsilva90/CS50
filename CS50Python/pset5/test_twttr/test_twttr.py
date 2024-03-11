df4a37207a94dc809bf67cfccf099a0271f2f884
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-11 19:22:22 +0000

    Mon, Mar 11, 2024, 7:22 PM +00:00

--- CS50Python/pset5/test_twttr/test_twttr.py
+++ CS50Python/pset5/test_twttr/test_twttr.py
@@ -3,6 +3,8 @@ from twttr import shorten
 def test_shorten():
     assert shorten("Hello, world!") == "Hll, wrld!"
     assert shorten("My name is...") == "My nm s..."
+    assert shorten("12345") == "12345"
+    assert shorten("ALL IN CAPSLOCK") == "LL N CPSLCK"
 
 
 
