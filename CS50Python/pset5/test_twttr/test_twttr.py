98c10f7e09e1ea2681b0d4809e00d2dbb27e626d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-11 19:18:48 +0000

    Mon, Mar 11, 2024, 7:18 PM +00:00

--- CS50Python/pset5/test_twttr/test_twttr.py
+++ CS50Python/pset5/test_twttr/test_twttr.py
@@ -1,2 +1,16 @@
 from twttr import shorten
 
+def test_shorten():
+    assert shorten("Hello, world!") == "Hll, wrld!"
+    assert shorten("My name is...") == "My nm s..."
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
