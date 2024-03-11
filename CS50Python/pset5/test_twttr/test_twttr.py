b2e5578384c9ea3fbb6733ec175f8cddfb77467c
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-11 19:28:39 +0000

    Mon, Mar 11, 2024, 7:28 PM +00:00

--- CS50Python/pset5/test_twttr/test_twttr.py
+++ CS50Python/pset5/test_twttr/test_twttr.py
@@ -6,6 +6,7 @@ def test_shorten():
     assert shorten("My name is...") == "My nm s..."
     assert shorten("12345") == "12345"
     assert shorten("ALL IN CAPSLOCK") == "LL N CPSLCK"
+    assert shorten("L33t 5p34k")
 
 
 if __name__ == "__main__":
