6da452c837c44f97bc9c2c3c1fd1dea85a3443fc
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-13 12:21:13 +0000

    Wed, Mar 13, 2024, 12:21 PM +00:00

--- CS50Python/pset6/lines/lines.py
+++ CS50Python/pset6/lines/lines.py
@@ -1,3 +1,12 @@
 import sys
 
-with open(argv[1])
+line_count = 0
+
+with open(argv[1]) as file:
+    for line in file:
+        line.strip()
+        if line.startswith('"""') or line.startswith("#"):
+            continue
+        else:
+            line_count += 1
+
