e312d934ab291aa262c2174912f6ad3d2a9c511f
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-13 12:30:36 +0000

    Wed, Mar 13, 2024, 12:30 PM +00:00

--- CS50Python/pset6/lines/lines.py
+++ CS50Python/pset6/lines/lines.py
@@ -4,9 +4,9 @@ def main():
 
     line_count = 0
 
-    if len(argv) == 2:
+    if len(sys.argv) == 2:
         try:
-            with open(argv[1]) as file:
+            with open(sys.argv[1]) as file:
                 for line in file:
                     line.strip()
                     if line.startswith('"""') or line.startswith("#"):
@@ -15,7 +15,7 @@ def main():
                         line_count += 1
         except FileNotFoundError:
             sys.exit("File does not exist")
-    elif len(argv) < 2:
+    elif len(sys.argv) < 2:
         sys.exit("Too few command-line arguments")
     else:
         sys.exit("Too many command-line arguments")
