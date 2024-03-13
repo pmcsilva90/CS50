b5f2bead857702ff35fd24e456485a62a3378ce5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-13 12:49:59 +0000

    Wed, Mar 13, 2024, 12:49 PM +00:00

--- CS50Python/pset6/lines/lines.py
+++ CS50Python/pset6/lines/lines.py
@@ -1,10 +1,16 @@
 import sys
 
+def is_python(filename):
+    if filename.endswith(".py"):
+        return True
+    return False
+
+
 def main():
 
     line_count = 0
 
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 2 and is_python(sys.argv[1]):
         try:
             with open(sys.argv[1]) as file:
                 for line in file:
