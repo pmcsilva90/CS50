99e95bebda3607450b2e923967ed4ec16c032618
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-14 15:37:22 +0000

    Thu, Mar 14, 2024, 3:37 PM +00:00

--- CS50Python/pset6/shirt/shirt.py
+++ CS50Python/pset6/shirt/shirt.py
@@ -1,6 +1,13 @@
 import sys
+import PIL
 
 #  two command-line arguments
 # in sys.argv[1], the name (or path) of a JPEG or PNG to read (i.e., open) as input
 # in sys.argv[2], the name (or path) of a JPEG or PNG to write (i.e., save) as output
 
+if len(sys.argv) < 3:
+    sys.exit("Too few arguments")
+if len(sys.argv) > 3:
+    sys.exit("Too many arguments")
+if len(sys.argv) == 3:
+    if 
