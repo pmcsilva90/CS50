854a998368a2f8739579108569927ab917a9d6bd
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-14 16:05:10 +0000

    Thu, Mar 14, 2024, 4:05 PM +00:00

--- CS50Python/pset6/shirt/shirt.py
+++ CS50Python/pset6/shirt/shirt.py
@@ -13,11 +13,16 @@ if len(sys.argv) > 3:
     sys.exit("Too many arguments")
 if len(sys.argv) == 3:
     if is_image(sys.argv[1]) and is_image(sys.argv[2]):
-        if sys.argv[1][]
-        try:
-            ...
-        except FileNotFoundError:
-            sys.exit("")
+        if splitext(sys.argv[1])[1] == splitext(sys.argv[2])[1]:
+            try:
+                with open(sys.argv[1]) as file:
+                    
+            except FileNotFoundError:
+                sys.exit("File not found")
+        else:
+            sys.exit("Input and output have different extensions")
+    else:
+        sys.exit("Invalid input")
 
 def is_image(filename):
     _, ext = splitext(filename)
