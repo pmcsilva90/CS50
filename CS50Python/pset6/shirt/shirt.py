9f03027bb955857123773be0d010b55c43c17004
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-14 16:06:19 +0000

    Thu, Mar 14, 2024, 4:06 PM +00:00

--- CS50Python/pset6/shirt/shirt.py
+++ CS50Python/pset6/shirt/shirt.py
@@ -7,22 +7,22 @@ def main():
 # in sys.argv[1], the name (or path) of a JPEG or PNG to read (i.e., open) as input
 # in sys.argv[2], the name (or path) of a JPEG or PNG to write (i.e., save) as output
 
-if len(sys.argv) < 3:
-    sys.exit("Too few arguments")
-if len(sys.argv) > 3:
-    sys.exit("Too many arguments")
-if len(sys.argv) == 3:
-    if is_image(sys.argv[1]) and is_image(sys.argv[2]):
-        if splitext(sys.argv[1])[1] == splitext(sys.argv[2])[1]:
-            try:
-                with open(sys.argv[1]) as file:
-                    
-            except FileNotFoundError:
-                sys.exit("File not found")
+    if len(sys.argv) < 3:
+        sys.exit("Too few arguments")
+    elif len(sys.argv) > 3:
+        sys.exit("Too many arguments")
+    elif len(sys.argv) == 3:
+        if is_image(sys.argv[1]) and is_image(sys.argv[2]):
+            if splitext(sys.argv[1])[1] == splitext(sys.argv[2])[1]:
+                try:
+                    with open(sys.argv[1]) as file:
+
+                except FileNotFoundError:
+                    sys.exit("File not found")
+            else:
+                sys.exit("Input and output have different extensions")
         else:
-            sys.exit("Input and output have different extensions")
-    else:
-        sys.exit("Invalid input")
+            sys.exit("Invalid input")
 
 def is_image(filename):
     _, ext = splitext(filename)
