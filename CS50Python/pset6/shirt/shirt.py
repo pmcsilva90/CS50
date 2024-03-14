57882ce723ebbf35ab9289157c63a6e67bcf6e9b
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-14 15:27:30 +0000

    Thu, Mar 14, 2024, 3:27 PM +00:00

--- CS50Python/pset6/shirt/shirt.py
+++ CS50Python/pset6/shirt/shirt.py
@@ -1,2 +1,6 @@
 import sys
 
+#  two command-line arguments
+# in sys.argv[1], the name (or path) of a JPEG or PNG to read (i.e., open) as input
+# in sys.argv[2], the name (or path) of a JPEG or PNG to write (i.e., save) as output
+
