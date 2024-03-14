6f8382c3c33e6f6f875951df960db60f1e67e662
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-14 15:43:19 +0000

    Thu, Mar 14, 2024, 3:43 PM +00:00

--- CS50Python/pset6/shirt/shirt.py
+++ CS50Python/pset6/shirt/shirt.py
@@ -1,6 +1,8 @@
 import sys
 import PIL
 
+def main():
+
 #  two command-line arguments
 # in sys.argv[1], the name (or path) of a JPEG or PNG to read (i.e., open) as input
 # in sys.argv[2], the name (or path) of a JPEG or PNG to write (i.e., save) as output
@@ -10,4 +12,15 @@ if len(sys.argv) < 3:
 if len(sys.argv) > 3:
     sys.exit("Too many arguments")
 if len(sys.argv) == 3:
-    if 
+    if is_image(sys.argv[1]) and is_image(sys.argv[2]):
+        try:
+            ...
+        except FileNotFoundError:
+            sys.exit("")
+
+def is_image(filename):
+    if filename.endswith(".png") or filename.endswith(".jpeg") or filename.endswith(".jpg"):
+        return True
+    return False
+
+
