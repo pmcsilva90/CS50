6f755e2531114ebfd05c3ed12905711e3c1a276a
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-15 19:54:37 +0000

    Fri, Mar 15, 2024, 7:54 PM +00:00

--- CS50Python/pset7/working/working.py
+++ CS50Python/pset7/working/working.py
@@ -9,7 +9,12 @@ def main():
 def convert(s):
     matches = re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) (to|-) (\d{1,2}):?(\d{2})? (AM|PM)", s)
     print(matches.groups())
+
+    start_h = matches.groups(1)
+    start_m = matches.groups(2)
+    start_ampm = matches.groups(3)
     
+
     return "done"
 
 
