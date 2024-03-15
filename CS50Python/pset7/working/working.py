07f7dda825458692d4af039cd05b713e17e2fc98
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-15 21:02:08 +0000

    Fri, Mar 15, 2024, 9:02 PM +00:00

--- CS50Python/pset7/working/working.py
+++ CS50Python/pset7/working/working.py
@@ -9,14 +9,10 @@ def main():
 def convert(s):
     matches = re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) (to|-) (\d{1,2}):?(\d{2})? (AM|PM)", s)
     print(matches.groups())
+    valid_time = False
 
-    for n in range(1, len(matches.groups()) + 1):
-        print(matches.group(n), type(matches.group(n)))
+    
 
-    if matches.group(2) == None:
-        print("just None works")
-
-    return "done"
 
 
 if __name__ == "__main__":
