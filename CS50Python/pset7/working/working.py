385b9e511cd27eecf1dc8c0d8d451e2dca376aa5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-15 20:32:36 +0000

    Fri, Mar 15, 2024, 8:32 PM +00:00

--- CS50Python/pset7/working/working.py
+++ CS50Python/pset7/working/working.py
@@ -11,13 +11,16 @@ def convert(s):
     print(matches.groups())
     valid_time = False
 
-    start_h = matches.groups(1)
-    start_m = matches.groups(2)
-    start_ampm = matches.groups(3)
-    to = matches.groups(4)
-    end_h = matches.groups(5)
-    end_m = matches.groups(6)
-    end_ampm = matches.groups(7)
+    start_h = int(matches.groups(1))
+    start_m = int(matches.groups(2))
+    start_ampm = str(matches.groups(3))
+    to = str(matches.groups(4)
+    end_h = int(matches.groups(5))
+    end_m = int(matches.groups(6)))
+    end_ampm = str(matches.groups(7))
+
+    for n in range(1, len(matches)):
+        print(type(matches.groups(n)), )
 
     if (0 < start_h < 13) and (0 < end_h < 13) and (0 <= start_m < 60) and (0 <= end_m < 60):
         valid_time = True
