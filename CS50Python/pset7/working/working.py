bf05cbf5c6dae432a103987a201ea5460f4390d5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-15 21:16:24 +0000

    Fri, Mar 15, 2024, 9:16 PM +00:00

--- CS50Python/pset7/working/working.py
+++ CS50Python/pset7/working/working.py
@@ -10,11 +10,26 @@ def convert(s):
     matches = re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) (to|-) (\d{1,2}):?(\d{2})? (AM|PM)", s)
     print(matches.groups())
     valid_time = False
+    start_h = int(matches.group(1))
+    start_m = int(matches.group(2))
+    start_ampm = matches.group(3)
+    to = matches.group(4)
+    end_h = int(matches.group(5))
+    end_m = int(matches.group(6))
+    end_ampm = matches.group(7)
 
-    if (0 < int(matches.group(1)) <= 12) and (0 < int(matches.group(5)) <= 12):
-        if matches.group(2) and matches.group(6):
-            if (0 <= int(matches.group(1)) < 60) and (0 <= int(matches.group(1)) < 60):
-                valid_time = True
+    if (0 < start_h <= 12) and (0 < end_h <= 12):
+        if (0 <= start_m < 60) and (0 <= end_m < 60):
+            valid_time = True
+    if start_ampm == "PM":
+        start_h += 12
+    if end_ampm == "PM":
+        end_h += 12
+    if to != "to":
+        sys.exit("to error")
+
+    return f"{}{}{}{}"
+
 
 
 
