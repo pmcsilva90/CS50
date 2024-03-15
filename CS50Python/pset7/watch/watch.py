198a80fe6bb0988dfaef3764de912d4c25d8d745
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-15 18:38:43 +0000

    Fri, Mar 15, 2024, 6:38 PM +00:00

--- CS50Python/pset7/watch/watch.py
+++ CS50Python/pset7/watch/watch.py
@@ -7,18 +7,10 @@ def main():
 
 
 def parse(s):
-    # <iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
-    # --> https://youtu.be/xvFZjo5PgG0
-    # <iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    # --> https://youtu.be/xvFZjo5PgG0
-    # <iframe width="560" height="315" src="https://cs50.harvard.edu/python"></iframe>
-    # --> None
-
     matches = re.search(r".+(youtube\.com/embed/\w+).+", s, re.IGNORECASE)
-    url = matches.group(1)
-    url = "https://" + url.replace("youtube.com/embed", "youtu.be")
-
     try:
+        url = matches.group(1)
+        url = "https://" + url.replace("youtube.com/embed", "youtu.be")
         return url
     except AttributeError:
         sys.exit("None")
