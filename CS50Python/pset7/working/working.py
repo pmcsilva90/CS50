b839102c563683461211f1f7290511ea3359be51
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-15 20:56:17 +0000

    Fri, Mar 15, 2024, 8:56 PM +00:00

--- CS50Python/pset7/working/working.py
+++ CS50Python/pset7/working/working.py
@@ -10,7 +10,7 @@ def convert(s):
     matches = re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) (to|-) (\d{1,2}):?(\d{2})? (AM|PM)", s)
     print(matches.groups())
 
-    for n in range(1, len(matches.groups())):
+    for n in range(1, len(matches.groups()) + 1):
         print(matches.group(n), type(matches.group(n)))
 
 
