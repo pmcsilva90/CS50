78494f4550398f3748a9299c02a921acd640f07d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-15 19:21:05 +0000

    Fri, Mar 15, 2024, 7:21 PM +00:00

--- CS50Python/pset7/working/working.py
+++ CS50Python/pset7/working/working.py
@@ -9,7 +9,8 @@ def main():
 def convert(s):
     matches = re.search(r"(\d{1,2}):?(\d{1,2}?) (to|-) (\d{1,2}):?(\d{1,2}?)", s, re.IGNORECASE)
     for n in range(1,4):
-        print(str(matches.group(n)))
+        print(matches.group(n))
+        return "done"
 
 
 if __name__ == "__main__":
