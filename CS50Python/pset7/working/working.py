519d2fc7a190a93cd995371b46be6652b9b4d286
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-15 19:18:33 +0000

    Fri, Mar 15, 2024, 7:18 PM +00:00

--- CS50Python/pset7/working/working.py
+++ CS50Python/pset7/working/working.py
@@ -7,10 +7,9 @@ def main():
 
 
 def convert(s):
-    matches = re.search(r"\d{1,2}:?\d{1,2}? ")
-
-
-...
+    matches = re.search(r"(\d{1,2}):?(\d{1,2}?) (to|-) (\d{1,2}):?(\d{1,2}?)")
+    for n in range(1,4):
+        print(matches.group(n))
 
 
 if __name__ == "__main__":
