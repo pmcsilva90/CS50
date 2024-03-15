c44e5116d297d4b42067fbb193c12f3c89e5f3fb
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-15 10:58:57 +0000

    Fri, Mar 15, 2024, 10:58 AM +00:00

--- CS50Python/pset7/numb3rs/numb3rs.py
+++ CS50Python/pset7/numb3rs/numb3rs.py
@@ -3,15 +3,19 @@ import sys
 
 
 def main():
-    #print(validate(input("IPv4 Address: ")))
-    validate(input("IPv4 Address: "))
+    print(validate(input("IPv4 Address: ")))
+    #validate(input("IPv4 Address: "))
 
 def validate(ip):
-    valid = True
+    valid = 0
     matches = re.search(r"^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)$", ip)
     for n in range(4):
         if 0 < int(matches.group(n)) < 256:
-            
+            valid += 1
+    if valid == 4:
+        return True
+    return False
+
 
 
 
