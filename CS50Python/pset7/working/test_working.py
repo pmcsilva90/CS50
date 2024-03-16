2f8f5cbdbd3facfc1bcb7c77375f6fa796945ab7
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-16 16:59:47 +0000

    Sat, Mar 16, 2024, 4:59 PM +00:00

--- CS50Python/pset7/working/test_working.py
+++ CS50Python/pset7/working/test_working.py
@@ -6,12 +6,12 @@ def test_case():
     assert convert("9 PM to 7 AM") == "21:00 to 07:00"
     assert convert("2 AM to 1 AM") == "02:00 to 01:00"
 
-    assert convert("05:31 pm to 10:31 AM") == ValueError
-    assert convert("02:35 AM to 09:30 am") == ValueError
-    assert convert("09:48 Pm to 09:16 Am") == ValueError
-    assert convert("06:29 aM to 12:51 AM") == ValueError
-    assert convert("02 am to 10 AM") == ValueError
-    assert convert("11 am to 4 am") == ValueError
+    assert convert("05:31 pm to 10:31 AM") == ValueError: Invalid format
+    assert convert("02:35 AM to 09:30 am") == ValueError: Invalid format
+    assert convert("09:48 Pm to 09:16 Am") == ValueError: Invalid format
+    assert convert("06:29 aM to 12:51 AM") == ValueError: Invalid format
+    assert convert("02 am to 10 AM") == ValueError: Invalid format
+    assert convert("11 am to 4 am") == ValueError: Invalid format
 
 
 def test_inrange():
@@ -20,21 +20,21 @@ def test_inrange():
     assert convert("10 AM to 4 PM") == "10:00 to 16:00"
     assert convert("10 PM to 12 PM") == "22:00 to 12:00"
 
-    assert convert("11:82 AM to 07:47 AM") == ValueError
-    assert convert("13:08 PM to 04:29 PM") == ValueError
-    assert convert("-2:31 AM to 10:06 AM") == ValueError
-    assert convert("04:53 PM to -6:57 AM") == ValueError
-    assert convert("06:23 PM to 02:60 PM") == ValueError
-    assert convert("08:41 PM to 24:15 PM") == ValueError
-    assert convert("2 AM to 16 PM") == ValueError
+    assert convert("11:82 AM to 07:47 AM") == ValueError: Invalid time
+    assert convert("13:08 PM to 04:29 PM") == ValueError: Invalid time
+    assert convert("-2:31 AM to 10:06 AM") == ValueError: Invalid time
+    assert convert("04:53 PM to -6:57 AM") == ValueError: Invalid time
+    assert convert("06:23 PM to 02:60 PM") == ValueError: Invalid time
+    assert convert("08:41 PM to 24:15 PM") == ValueError: Invalid time
+    assert convert("2 AM to 16 PM") == ValueError: Invalid time
 
 
 def test_format():
-    assert convert("02:56PM to 03:2 AM") == ValueError
-    assert convert("7 AM - 5 AM") == ValueError
-    assert convert("07:5 PM to 12:4 PM") == ValueError
-    assert convert("8PM - 3PM") == ValueError
-    assert convert("08:13 AM until 01:03 PM") == ValueError
+    assert convert("02:56PM to 03:2 AM") == ValueError: Invalid format
+    assert convert("7 AM - 5 AM") == ValueError: Invalid format
+    assert convert("07:5 PM to 12:4 PM") == ValueError: Invalid format
+    assert convert("8PM - 3PM") == ValueError: Invalid format
+    assert convert("08:13 AM until 01:03 PM") == ValueError: Invalid format
 
 if __name__ == "__main__":
     main()
