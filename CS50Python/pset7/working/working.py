085be88e6f63a6b06bfc004bf4c88734ef398fd1
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-16 14:58:59 +0000

    Sat, Mar 16, 2024, 2:58 PM +00:00

--- CS50Python/pset7/working/working.py
+++ CS50Python/pset7/working/working.py
@@ -8,7 +8,7 @@ def main():
 
 def convert(s):
     if matches := re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) (to|-) (\d{1,2}):?(\d{2})? (AM|PM)", s):
-        
+        start_h, start_m, start_ampm, 
 
 
 
