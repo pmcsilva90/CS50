4cdbebf2d53b6402f602a9b8a847dd1dc79082bf
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-16 15:30:00 +0000

    Sat, Mar 16, 2024, 3:30 PM +00:00

--- CS50Python/pset7/working/working.py
+++ CS50Python/pset7/working/working.py
@@ -10,16 +10,17 @@ def convert(s):
     if matches := re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) (to|-) (\d{1,2}):?(\d{2})? (AM|PM)", s):
         start_h, start_m, start_ampm, prep, end_h, end_m, end_ampm = matches.groups()
 
-        if start_h == 0 or end_h == 0:
-            raise ValueError("Invalid time")
-
         start_h = int(start_h)
+        if not 0 < start_h <= 12:
+            raise ValueError("Invalid time")
         if start_ampm == "PM" and start_h < 12:
             start_h += 12
         elif start_ampm == "AM" and start_h == 12:
             start_h = 0
 
         end_h = int(end_h)
+        if not 0 < end_h <= 12:
+            raise ValueError("Invalid time")
         if end_ampm == "PM" and end_h < 12:
             end_h += 12
         elif end_ampm == "AM" and end_h == 12:
