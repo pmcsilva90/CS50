32c6ea8405600e4e0f3a4684378216b84b817562
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-16 17:32:03 +0000

    Sat, Mar 16, 2024, 5:32 PM +00:00

--- CS50Python/pset7/working/test_working.py
+++ CS50Python/pset7/working/test_working.py
@@ -12,31 +12,28 @@ def test_case():
     assert convert("2 AM to 1 AM") == "02:00 to 01:00"
 
     try:
-        convert("05:31 pm to 10:31 AM")
+        assert convert("05:31 pm to 10:31 AM")
     except ValueError
 
     try:
-        convert("02:35 AM to 09:30 am")
+        assert convert("02:35 AM to 09:30 am")
     except ValueError
 
     try:
-        convert("09:48 Pm to 09:16 Am")
+        assert convert("09:48 Pm to 09:16 Am")
     except ValueError
 
     try:
-        convert("06:29 aM to 12:51 AM")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+        assert convert("06:29 aM to 12:51 AM")
+    except ValueError
 
     try:
-        convert("02 am to 10 AM")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+        assert convert("02 am to 10 AM")
+    except ValueError
 
     try:
-        convert("11 am to 4 am")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+        assert convert("11 am to 4 am")
+    except ValueError
 
 
 def test_inrange():
@@ -46,66 +43,54 @@ def test_inrange():
     assert convert("10 PM to 12 PM") == "22:00 to 12:00"
 
     try:
-        convert("11:82 AM to 07:47 AM")
-    except ValueError as e:
-        assert str(e) == "Invalid time"
+        assert convert("11:82 AM to 07:47 AM")
+    except ValueError
 
     try:
-        convert("13:08 PM to 04:29 PM")
-    except ValueError as e:
-        assert str(e) == "Invalid time"
+        assert convert("13:08 PM to 04:29 PM")
+    except ValueError
 
     try:
-        convert("-2:31 AM to 10:06 AM")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+        assert convert("-2:31 AM to 10:06 AM")
+    except ValueError
 
     try:
-        convert("04:53 PM to -6:57 AM")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+        assert convert("04:53 PM to -6:57 AM")
+    except ValueError
 
     try:
-        convert("06:23 PM to 02:60 PM")
-    except ValueError as e:
-        assert str(e) == "Invalid time"
+        assert convert("06:23 PM to 02:60 PM")
+    except ValueError
 
     try:
-        convert("08:41 PM to 24:15 PM")
-    except ValueError as e:
-        assert str(e) == "Invalid time"
+        assert convert("08:41 PM to 24:15 PM")
+    except ValueError
 
     try:
-        convert("2 AM to 16 PM")
-    except ValueError as e:
-        assert str(e) == "Invalid time"
+        assert convert("2 AM to 16 PM")
+    except ValueError
 
 
 def test_format():
     try:
         convert("02:56PM to 03:2 AM")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+    except ValueError
 
     try:
         convert("7 AM - 5 AM")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+    except ValueError
 
     try:
         convert("07:5 PM to 12:4 PM")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+    except ValueError
 
     try:
         convert("8PM - 3PM")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+    except ValueError
 
     try:
         convert("08:13 AM until 01:03 PM")
-    except ValueError as e:
-        assert str(e) == "Invalid format"
+    except ValueError
 
 if __name__ == "__main__":
     main()
