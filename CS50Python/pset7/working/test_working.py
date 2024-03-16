324c90482dd25a7e5361973fbed1ac0388869e91
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-16 16:42:34 +0000

    Sat, Mar 16, 2024, 4:42 PM +00:00

--- CS50Python/pset7/working/test_working.py
+++ CS50Python/pset7/working/test_working.py
@@ -1,14 +1,14 @@
 from working import convert
 
 def test_case():
-    assert convert("12:01 AM to 07:08 PM") == "12:01 to 19:08"
-    assert convert("10:00 PM to 02:40 PM") == "10:00 to 14:40"
-    assert convert("9 PM to 7 AM") == "9 PM to 7 AM"
-    assert convert("2 AM to 1 AM") == "2 AM to 1 AM"
+    assert convert("12:01 AM to 07:08 PM") == True
+    assert convert("10:00 PM to 02:40 PM") == True
+    assert convert("9 PM to 7 AM") == True
+    assert convert("2 AM to 1 AM") == True
 
-    assert convert("05:31 pm to 10:31 AM") == "05:31 pm to 10:31 AM"
-    assert convert("02:35 AM to 09:30 am") == "02:35 AM to 09:30 am"
-    assert convert("09:48 Pm to 09:16 Am") == "09:48 Pm to 09:16 Am"
+    assert convert("05:31 pm to 10:31 AM") == False
+    assert convert("02:35 AM to 09:30 am") == False
+    assert convert("09:48 Pm to 09:16 Am") == False
     assert convert("06:29 aM to 12:51 AM") == False
     assert convert("02 am to 10 AM") == False
     assert convert("11 am to 4 am") == False
@@ -19,7 +19,6 @@ def test_inrange():
     assert convert("07:31 PM to 11:26 AM") == True
     assert convert("10 AM to 4 PM") == True
     assert convert("10 PM to 12 PM") == True
-
 
     assert convert("11:82 AM to 07:47 AM") == False
     assert convert("13:08 PM to 04:29 PM") == False
