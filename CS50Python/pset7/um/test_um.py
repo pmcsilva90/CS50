f20c05cb1175fc20eddba0f5ed1cf25fe7790546
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-17 16:28:22 +0000

    Sun, Mar 17, 2024, 4:28 PM +00:00

--- CS50Python/pset7/um/test_um.py
+++ CS50Python/pset7/um/test_um.py
@@ -1,4 +1,29 @@
 from um import count
 
 def main():
-    test_
+    test_inwords()
+    test_spaces()
+    test_case()
+
+def test_inwords():
+    assert count("album") == 0
+    assert count("Do you play the drums?") == 0
+    assert count("Hello, um friend") == 1
+    assert count("123um") == 0
+    assert count("??um??") == 1
+
+
+def test_spaces():
+    assert count("Hello, um... name?") == 1
+    assert count("Do you require um, space?") == 1
+    assert count("Just um a bit") == 1
+
+
+def test_case():
+    assert count("WHY ARE WE, UM, YELLING?") == 1
+    assert count("Um, I don't know") == 1
+    assert count("I'll yell, uM, JUST A BIT") == 1
+
+if __name__ == "__main__":
+    main()
+
