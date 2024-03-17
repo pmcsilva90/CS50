10d254b8560b2f7ef5fbb5524dd1451c84b5c8b4
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-17 16:45:59 +0000

    Sun, Mar 17, 2024, 4:45 PM +00:00

--- CS50Python/pset7/response/response.py
+++ CS50Python/pset7/response/response.py
@@ -1,9 +1,13 @@
 import validators
 
-s = input("What's your email address? ")
+def main():
 
+    s = input("What's your email address? ")
 
-if validators.email(s):
-    print("Valid")
-else:
-    print("Invalid")
+    if validators.email(s):
+        print("Valid")
+    else:
+        print("Invalid")
+
+if __name__ == "__main__":
+    main()
