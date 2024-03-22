329969e7e10446e6ff85511dbb47b5e27f5d8787
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-22 12:48:24 +0000

    Fri, Mar 22, 2024, 12:48 PM +00:00

--- CS50Python/pset8/seasons/seasons.py
+++ CS50Python/pset8/seasons/seasons.py
@@ -3,11 +3,11 @@ from datetime import date
 
 def main():
     mybday = date(1990, 3, 18)
-    tday = date.today()
-    elapsed = tday - mybday
+    mybdaypl1 = date(1991, 3, 18)
+    elapsed = mybdaypl1 - mybday
 
     print(mybday)
-    print(tday)
+    print(mybdaypl1)
     print(elapsed.total_seconds() / 60)
 
 
