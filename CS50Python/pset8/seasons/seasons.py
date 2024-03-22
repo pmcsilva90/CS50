8f7dfa9a61704f1680e54862ae20f83f794f30f0
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-22 13:08:12 +0000

    Fri, Mar 22, 2024, 1:08 PM +00:00

--- CS50Python/pset8/seasons/seasons.py
+++ CS50Python/pset8/seasons/seasons.py
@@ -1,5 +1,6 @@
 from datetime import date
 import inflect
+import re
 
 p = inflect.engine()
 
@@ -18,7 +19,7 @@ def main():
     dob = input("Date of birth: ")
 
 def valid_dob(s):
-    
+    if 
 
 
 
