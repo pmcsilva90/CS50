df3e520da648d81c85cfc2c363e8c1659faf62ab
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-22 13:02:41 +0000

    Fri, Mar 22, 2024, 1:02 PM +00:00

--- CS50Python/pset8/seasons/seasons.py
+++ CS50Python/pset8/seasons/seasons.py
@@ -5,15 +5,17 @@ p = inflect.engine()
 
 
 def main():
-    mybday = date(1990, 3, 18)
-    mybdaypl1 = date(1992, 3, 18)
-    elapsed = mybdaypl1 - mybday
-    elapsed_minutes = elapsed.total_seconds() / 60
+    # mybday = date(1990, 3, 18)
+    # mybdaypl1 = date(1992, 3, 18)
+    # elapsed = mybdaypl1 - mybday
+    # elapsed_minutes = elapsed.total_seconds() / 60
 
-    print(mybday)
-    print(mybdaypl1)
-    print(elapsed_minutes)
-    print(p.number_to_words(int(elapsed_minutes), andword=""))
+    # print(mybday)
+    # print(mybdaypl1)
+    # print(elapsed_minutes)
+    # print(p.number_to_words(int(elapsed_minutes), andword="").capitalize())
+
+    
 
 
 if __name__ == "__main__":
