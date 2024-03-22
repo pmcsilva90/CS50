b964e6345f1629172ffe51293a3ce7409e4e6a55
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-22 12:49:28 +0000

    Fri, Mar 22, 2024, 12:49 PM +00:00

--- CS50Python/pset8/seasons/seasons.py
+++ CS50Python/pset8/seasons/seasons.py
@@ -5,10 +5,11 @@ def main():
     mybday = date(1990, 3, 18)
     mybdaypl1 = date(1991, 3, 18)
     elapsed = mybdaypl1 - mybday
+    elapsed_minutes = elapsed.total_seconds() / 60
 
     print(mybday)
     print(mybdaypl1)
-    print(elapsed.total_seconds() / 60)
+    print(elapsed_minutes)
 
 
 if __name__ == "__main__":
