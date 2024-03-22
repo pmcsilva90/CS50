b7d080e3dad5d3c9e69d4798e8268f69050a1885
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-22 13:32:57 +0000

    Fri, Mar 22, 2024, 1:32 PM +00:00

--- CS50Python/pset8/seasons/seasons.py
+++ CS50Python/pset8/seasons/seasons.py
@@ -7,22 +7,19 @@ p = inflect.engine()
 
 
 def main():
-    # mybday = date(1990, 3, 18)
-    # mybdaypl1 = date(1992, 3, 18)
-    # elapsed = mybdaypl1 - mybday
-    # elapsed_minutes = elapsed.total_seconds() / 60
-
-    # print(mybday)
-    # print(mybdaypl1)
-    # print(elapsed_minutes)
-    # print(p.number_to_words(int(elapsed_minutes), andword="").capitalize())
 
     dob_input = validate_dob(input("Date of birth: "))
 
+    # Convert to date format
     dob_date = date(dob_input[0], dob_input[1], dob_input[2])
+
+    # Calculate delta time in minutes
     elapsed = date.today() - dob_date
     elapsed_minutes = int(elapsed.total_seconds() / 60)
+
+    # Convert to words
     elapsed_words = p.number_to_words(elapsed_minutes, andword="").capitalize() + " minutes"
+
     print(elapsed_words)
 
 def validate_dob(s):
@@ -31,9 +28,6 @@ def validate_dob(s):
         return int(year), int(month), int(day)
     else:
         sys.exit("Invalid date")
-
-
-
 
 
 if __name__ == "__main__":
