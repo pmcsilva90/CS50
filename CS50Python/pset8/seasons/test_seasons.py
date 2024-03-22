38a510c1be43b32feef5ccbcb086a41dce714d80
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-22 15:42:23 +0000

    Fri, Mar 22, 2024, 3:42 PM +00:00

--- CS50Python/pset8/seasons/test_seasons.py
+++ CS50Python/pset8/seasons/test_seasons.py
@@ -7,6 +7,22 @@ def main():
 
 
 def test_dateformat():
-    with pytest.raises(SystemExit)
-    # assert validate_dob("") ==
-    # assert validate_dob("") ==
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("19-03-1999") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("1-1-2001") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("10-5-2000") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("5-25-1998") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("18 03 1990") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("1994 06 08") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("1983, 11, 17") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("July 4th 1984") == "Invalid date"
+
+    assert seasons.validate_dob("2001-02-25") == date(2001, 2, 25)
+
