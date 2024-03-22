e9c07971cce48b15e4e2cb8dfb9419614c844e66
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-22 16:03:11 +0000

    Fri, Mar 22, 2024, 4:03 PM +00:00

--- CS50Python/pset8/seasons/test_seasons.py
+++ CS50Python/pset8/seasons/test_seasons.py
@@ -4,7 +4,9 @@ from datetime import date
 
 def main():
     test_dateformat()
+    test_invalid_date()
     test_valid_date()
+    test_main()
 
 
 def test_dateformat():
@@ -25,10 +27,35 @@ def test_dateformat():
     with pytest.raises(SystemExit):
         assert seasons.validate_dob("July 4th 1984") == "Invalid date"
 
+
+def test_valid_date():
     assert seasons.validate_dob("2001-02-25") == date(2001, 2, 25)
     assert seasons.validate_dob("1990-03-17") == date(1990, 3, 17)
     assert seasons.validate_dob("1961-06-26") == date(1961, 6, 26)
     assert seasons.validate_dob("1980-02-01") == date(1980, 2, 1)
 
-def test_valid_date():
-    with pyet
+
+def test_invalid_date():
+    with pytest.raises(SystemExit):
+        assert seasons.main() == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("1974-13-25") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("1997-04-31") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("1945-07-32") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("999-01-02") == "Invalid date"
+
+def test_main():
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("random string") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("L3375p34k") == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert seasons.validate_dob("2000BC-01-01") == "Invalid date"
+    assert seasons.main()
+
+
+
+
