7cda1949fa50e687a709b22cf105b656bd7bd419
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-23 21:34:43 +0000

    Sat, Mar 23, 2024, 9:34 PM +00:00

--- CS50Python/pset8/shirtificate/shirtificate.py
+++ CS50Python/pset8/shirtificate/shirtificate.py
@@ -8,10 +8,16 @@ def main():
     pdf.add_page()
     pdf.image("shirtificate.png", (pdf.w - 180) / 2, 80, w=180)
 
-    pdf.set_font('helvetica', size=26, style='B')
-    pdf.set_text_color(0, 255, 255)
 
+    pdf.set_font('helvetica', size=40, style='B')
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(pdf.epw, 0, text="CS50 Shirtificate", align='C')
+
+
+    pdf.set_font('helvetica', size=26, style='B')
+    pdf.set_text_color(255, 255, 255)
     pdf.cell(pdf.epw, 260, text=f"{name} took CS50", align='C')
+
     pdf.output("hello_world.pdf")
 
 if __name__ == "__main__":
