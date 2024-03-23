49548142a54f9a9780299dac74364a8ee105a631
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-23 20:21:39 +0000

    Sat, Mar 23, 2024, 8:21 PM +00:00

--- CS50Python/pset8/shirtificate/shirtificate.py
+++ CS50Python/pset8/shirtificate/shirtificate.py
@@ -3,6 +3,6 @@ from fpdf import FPDF
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font('helvetica', size=12)
-pdf.image("shirtificate.png", w=210)
+pdf.image("shirtificate.png", pdf.eph, w=pdf.epw, )
 pdf.cell(text="hello world")
 pdf.output("hello_world.pdf")
