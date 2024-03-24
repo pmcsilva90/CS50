387e9275eaf16d6e17900aa45d4130a998855b95
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-24 18:25:03 +0000

    Sun, Mar 24, 2024, 6:25 PM +00:00

--- CS50Python/pset4/figlet/figlet.py
+++ CS50Python/pset4/figlet/figlet.py
@@ -12,6 +12,7 @@ if len(sys.argv) == 0:
 
 elif len(sys.argv) == 2:
     # argv[1] is "-f" or "--font", argv[2] is fontName
+    if sys.argv[1] == "-f" or sys.argv[1] == 
 else:
     sys.exit("Invalid usage")
 
