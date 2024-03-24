e294fd7145d3b53f5728949d6052292deddf43a2
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-24 18:55:32 +0000

    Sun, Mar 24, 2024, 6:55 PM +00:00

--- CS50Python/pset4/figlet/figlet.py
+++ CS50Python/pset4/figlet/figlet.py
@@ -3,26 +3,30 @@ import random
 from pyfiglet import Figlet
 
 figlet = Figlet()
-fonts = figlet.getFonts()
 
 
-if len(sys.argv) == 1:
-    f = random.choice(fonts)
-    print(f)
-    figlet.setFont(font=f)
-elif len(sys.argv) == 3:
-    f = sys.argv[2]
-    if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-        if f in fonts:
-            figlet.setFont(font=f)
+def main():
+    fonts = figlet.getFonts()
+    if len(sys.argv) == 1:
+        f = random.choice(fonts)
+        print(f)
+        figlet.setFont(font=f)
+    elif len(sys.argv) == 3:
+        f = sys.argv[2]
+        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+            if f in fonts:
+                figlet.setFont(font=f)
+            else:
+                sys.exit("Invalid font")
         else:
-            sys.exit("Invalid font")
+            sys.exit("Invalid usage")
     else:
         sys.exit("Invalid usage")
-else:
-    sys.exit("Invalid usage")
 
-str = input("Input: ")
+    str = input("Input: ")
 
-print("Output:")
-print(figlet.renderText(str))
+    print("Output:")
+    print(figlet.renderText(str))
+
+if __name__ == "__main__":
+    main()
