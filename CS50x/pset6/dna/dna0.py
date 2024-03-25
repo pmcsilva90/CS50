4118114cf47f7cf7384702c193899126087faab7
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-25 16:43:47 +0000

    Mon, Mar 25, 2024, 4:43 PM +00:00

--- CS50x/pset6/dna/dna0.py
+++ CS50x/pset6/dna/dna0.py
@@ -9,8 +9,9 @@ subseqs = []
 
 for i in range(seq_length):
     subseq = seq[start + i:end + i]
-    for j in range(seq_length)
-        
+    for j + 1 in range(seq_length)
+        subseq2 = seq[start + ]
+
 
 print(subseqs)
 print(seq_length)
