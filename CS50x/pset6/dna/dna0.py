e9b3439e7ff37bf4cb28836c4e4257863a44e942
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-25 16:36:22 +0000

    Mon, Mar 25, 2024, 4:36 PM +00:00

--- CS50x/pset6/dna/dna0.py
+++ CS50x/pset6/dna/dna0.py
@@ -5,8 +5,12 @@ seq_length = len(seq)
 start = 0
 end = start + 4
 
+subseqs = []
+
 for i in seq:
     subseq = seq[start:end]
-        if 
+    if subseq in seq:
+        subseqs.append(subseq)
 
+print(subseqs)
 print(seq_length)
