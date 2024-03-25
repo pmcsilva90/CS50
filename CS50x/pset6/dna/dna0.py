aca421a0933b861be5f0d10aa7134c57dbbd88c7
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-25 16:38:28 +0000

    Mon, Mar 25, 2024, 4:38 PM +00:00

--- CS50x/pset6/dna/dna0.py
+++ CS50x/pset6/dna/dna0.py
@@ -7,8 +7,8 @@ end = start + 4
 
 subseqs = []
 
-for i in seq:
-    subseq = seq[start:end]
+for i in range(seq_length):
+    subseq = seq[start + i:end + i]
     if subseq in seq:
         subseqs.append(subseq)
 
