6cec2fd42094e5c9039de35466ad9cc27a219e03
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-25 18:12:05 +0000

    Mon, Mar 25, 2024, 6:12 PM +00:00

--- CS50x/pset6/dna/dna0.py
+++ CS50x/pset6/dna/dna0.py
@@ -4,11 +4,8 @@ seq_length = len(seq)
 
 groups = []
 
-for i in range(seq_length):
-    subseq = {"subseq": seq[i]: "count": 0}
-    for j in range(seq_length(1, seq_length + 1)):
-        if seq[i] == seq[j] and seq[i] not in groups:
-            groups.append(["subseq": seq[i] "count"])
+for i in range(seq_length)
+    
 
 
 
