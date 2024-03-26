445d37c511445a6028df46b17d26f67ca8e2307c
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-26 18:33:08 +0000

    Tue, Mar 26, 2024, 6:33 PM +00:00

--- CS50SQL/pset0/views/2.sql
+++ CS50SQL/pset0/views/2.sql
@@ -1,1 +1,1 @@
-SELECT "average_color" from "views" WHERE "artist" = 'Hokusai' AND "english_title" LIKE '%river%';
+SELECT "average_color" FROM "views" WHERE "artist" = 'Hokusai' AND "english_title" LIKE '%river%';
