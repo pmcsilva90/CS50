c9d3c2469db7c16810df02b42d2f3565cfb0455a
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-29 16:53:12 +0000

    Fri, Mar 29, 2024, 4:53 PM +00:00

--- CS50x/pset7/fiftyville/log.sql
+++ CS50x/pset7/fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Find crime scene description
+SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2023 AND street = 'Humphrey Street';
