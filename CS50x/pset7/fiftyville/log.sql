a3fe403001bd9edaf64647032c85bc5d5e4b9261
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-29 17:20:56 +0000

    Fri, Mar 29, 2024, 5:20 PM +00:00

--- CS50x/pset7/fiftyville/log.sql
+++ CS50x/pset7/fiftyville/log.sql
@@ -1,11 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- Find crime scene description
+-- Find crime scene description.
 SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2023 AND street = 'Humphrey Street';
 
 -- "Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery."
 
--- Find mentions of 'bakery' in interviews
+-- Find mentions of 'bakery' in interviews.
 SELECT * FROM interviews WHERE transcript LIKE '%bakery%';
 
 -- Given this description from an Ruth's interview:
@@ -15,10 +15,12 @@ SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2023 
 
 -- Given this description from Eugene's interview:
 -- "I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money."
--- Find withdrawal transactions on 2023-07-28 on Leggett Street
+-- Find withdrawal transactions on 2023-07-28 on Leggett Street.
 SELECT * FROM atm_transactions WHERE day = 28 AND month = 7 AND year = 2023 AND transaction_type = 'withdraw' AND atm_location = 'Leggett Street';
 
 -- Given this description from Raymond's interview:
 -- "As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket."
---
+-- Find phone calls on 2023-07-28 of a duration of less than 60 seconds.
+SELECT * FROM phone_calls WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60;
+
 
