332e90f4c2202f378440448f4c8cc235cbea14c5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-29 17:08:12 +0000

    Fri, Mar 29, 2024, 5:08 PM +00:00

--- CS50x/pset7/fiftyville/log.sql
+++ CS50x/pset7/fiftyville/log.sql
@@ -8,9 +8,13 @@ SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2023 A
 -- Find mentions of 'bakery' in interviews
 SELECT * FROM interviews WHERE transcript LIKE '%bakery%';
 
--- Given this description from an interview:
+-- Given this description from an Ruth's interview:
 -- "Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame."
 -- Check bakery security logs of the day of the crime between 10:15am and 10:25am
 SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2023 AND hour = 10 AND minute BETWEEN 15 and 25;
+
+-- Given this description from Eugene's interview:
+-- "I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money."
+-- 
 
 
