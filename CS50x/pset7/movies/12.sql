1b44233a1e77b3e4a47a8644a2af71f515604cf4
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-29 16:16:17 +0000

    Fri, Mar 29, 2024, 4:16 PM +00:00

--- CS50x/pset7/movies/12.sql
+++ CS50x/pset7/movies/12.sql
@@ -1,8 +1,8 @@
-find Bradely Cooper id
-
-sqlite> SELECT id FROM people WHERE name = 'Bradley Cooper';
-
-find movies he stars in
-
+SELECT title FROM movies WHERE id IN (
+    SELECT movie_id FROM stars WHERE person_id IN (
+        SELECT id FROM people WHERE name = 'Bradley Cooper'))
+AND id IN (
+    SELECT movie_id FROM stars WHERE person_id IN (
+        SELECT id FROM people WHERE name = 'Jennifer Lawrence'));
 
 
