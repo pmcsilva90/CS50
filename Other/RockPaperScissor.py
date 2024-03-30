f97e2b1075c6f46b897afde8a738c1af21f88d17
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-30 20:36:00 +0000

    Sat, Mar 30, 2024, 8:36 PM +00:00

--- Other/RockPaperScissor.py
+++ Other/RockPaperScissor.py
@@ -1,19 +1,12 @@
-"""
-            rock    paper   scissors
-rock          0       1        -1
-paper         -1      0
-scissors                       0
+def main():
 
-r -1
-p 0
-s 1
+while True
+    p1 = input("player 1: ").lower()
+    p2 = input("player 2: ").lower()
 
+    if p1 not in ["rock", "paper", "scissors"] and p2 not in ["rock", "paper", "scissors"]
+        continue
 
-"""
-
-rock = -1
-paper = 0
-scissors = 1
 
 def outcome(p1, p2):
 
