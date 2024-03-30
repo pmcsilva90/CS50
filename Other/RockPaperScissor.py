c3d0ee78f9a8857a7b4722f96297fd18429f1e1e
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-30 20:46:27 +0000

    Sat, Mar 30, 2024, 8:46 PM +00:00

--- Other/RockPaperScissor.py
+++ Other/RockPaperScissor.py
@@ -1,40 +1,48 @@
 def main():
 
-while True
-    p1 = input("player 1: ").lower()
-    if p1 not in ["rock", "paper", "scissors"]
-        continue
-    else
-        break
+    while True:
+        p1 = input("player 1: ").lower()
+        if p1 not in ["rock", "paper", "scissors"]:
+            continue
+        else:
+            match p1:
+                case "rock":
+                    p1 = -1
+                    break
+                case "paper":
+                    p1 = 0
+                    break
+                case "scissors":
+                    p1 = 1
+                    break
+            break
 
-while True
-    p2 = input("player 2: ").lower()
-    if p2 not in ["rock", "paper", "scissors"]
-        continue
-    else
-        break
+    while True:
+        p2 = input("player 2: ").lower()
+        if p2 not in ["rock", "paper", "scissors"]:
+            continue
+        else:
+            match p2:
+                case "rock":
+                    p2 = -1
+                    break
+                case "paper":
+                    p2 = 0
+                    break
+                case "scissors":
+                    p2 = 1
+                    break
+            break
 
-
+    print(outcome)
 
 def outcome(p1, p2):
 
-    if p1 - p2 == 0
+    if p1 - p2 == 0:
         return "draw"
-    elif p1 - p2 == 1
+    elif p1 - p2 == 1:
         return "player 1 wins"
-    elif p1 - p2 != 1
+    else:
         return "player 2 wins"
 
-while True
-    p1 = input("player 1: ").lower()
-    p2 = input("player 2: ").lower()
-
-
-
-    if p1 not in ["rock", "paper", "scissors"] and p2 not in ["rock", "paper", "scissors"]
-        continue
-    elif p1 == p2
-        print("draw")
-    else
-        if p1 == "rock" and p2 == "paper"
-            print()
+main()
