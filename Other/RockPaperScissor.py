49721ec432da272170d8e8e479789e1e50922f4d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-30 20:06:41 +0000

    Sat, Mar 30, 2024, 8:06 PM +00:00

--- Other/RockPaperScissor.py
+++ Other/RockPaperScissor.py
@@ -1,8 +1,20 @@
+"""
+            rock    paper   scissors
+rock          0       1        -1
+paper         -1      0
+scissors                       0
 
+
+"""
 
 while True
     p1 = input("player 1: ").lower()
     p2 = input("player 2: ").lower()
 
+    
     if p1 not in ["rock", "paper", "scissors"] and p2 not in ["rock", "paper", "scissors"]
         continue
+    elif p1 == p2
+        print("Draw")
+    elif p1 == "rock" AND p2 == "scissors"
+        print()
