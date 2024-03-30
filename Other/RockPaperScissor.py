65565bc893015bf34456a81553b73f1abf5c2499
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-03-30 20:26:41 +0000

    Sat, Mar 30, 2024, 8:26 PM +00:00

--- Other/RockPaperScissor.py
+++ Other/RockPaperScissor.py
@@ -16,7 +16,9 @@ paper = 2
 scissors = 3
 
 def outcome(p1, p2):
-    if p1 - p2 = -1
+    if p1 - p2 = 0
+        return "draw"
+    elif p1 - p2 
 
 while True
     p1 = input("player 1: ").lower()
