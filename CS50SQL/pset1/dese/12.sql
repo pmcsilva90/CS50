c304ecb0be083e156e2ddf057580a9b93a1e331b
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 18:57:32 +0000

    Sat, Apr 6, 2024, 7:57 PM +01:00

--- CS50SQL/pset1/dese/12.sql
+++ CS50SQL/pset1/dese/12.sql
@@ -1,6 +1,8 @@
 -- public school districts with above-average per-pupil expenditures
 
--- SELECT * FROM districts JOIN expenditures ON districts.id = expenditures.district_id WHERE type = "Public School District" AND per_pupil_expenditure >
--- (SELECT AVG(per_pupil_expenditure) FROM expenditures);
+SELECT * FROM districts JOIN expenditures ON districts.id = expenditures.district_id WHERE type = "Public School District" AND per_pupil_expenditure >
+(SELECT AVG(per_pupil_expenditure) FROM expenditures);
 
+SELECT * FROM districts JOIN expenditures ON districts.id = expenditures.district_id JOIN staff_evaluations ON districts.id = staff_evaluations.district_id WHERE type = "Public School District" AND per_pupil_expenditure >
+(SELECT AVG(per_pupil_expenditure) FROM expenditures);
 
