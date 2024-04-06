bac40ca78ec93b107fe161a16059c05d457290ad
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 17:45:29 +0000

    Sat, Apr 6, 2024, 6:45 PM +01:00

--- CS50SQL/pset1/dese/5.sql
+++ CS50SQL/pset1/dese/5.sql
@@ -1,3 +1,4 @@
 SELECT * FROM (
-    SELECT city, COUNT(*) AS "Number of Public Schools" FROM schools WHERE type = "Public School" GROUP BY city ORDER BY COUNT(*) DESC, city)
+    SELECT city, COUNT(*) AS "Number of Public Schools" FROM schools
+    WHERE type = "Public School" GROUP BY city ORDER BY COUNT(*) DESC, city)
 WHERE "Number of Public Schools" <= 3;
