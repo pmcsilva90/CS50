2a7e298bbd9b920be3d9360924974b953957e3a0
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 19:25:01 +0000

    Sat, Apr 6, 2024, 8:25 PM +01:00

--- CS50SQL/pset1/dese/3.sql
+++ CS50SQL/pset1/dese/3.sql
@@ -1,4 +1,4 @@
 SELECT
-    ROUND(AVG(per_pupil_expenditure), 2) AS "Average District Per Pupil Expenditure"
+    AVG(per_pupil_expenditure) AS "Average District Per Pupil Expenditure"
 FROM
     expenditures;
