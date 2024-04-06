d0bfbf7b52b5e58cba308e11fcb6b30a14a943f5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 19:11:36 +0000

    Sat, Apr 6, 2024, 8:11 PM +01:00

--- CS50SQL/pset1/dese/13.sql
+++ CS50SQL/pset1/dese/13.sql
@@ -1,1 +1,4 @@
-
+SELECT name, needs_improvement, unsatisfactory, per_pupil_expenditure FROM districts
+JOIN staff_evaluations ON districts.id = staff_evaluations.district_id
+JOIN expenditures ON districts.id = expenditures.district_id
+ORDER BY per_pupil_expenditure;
