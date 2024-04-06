6c084c63a1d0ab69d304612e9e408c89202ad88d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 14:15:13 +0000

    Sat, Apr 6, 2024, 3:15 PM +01:00

--- CS50SQL/pset1/packages/log.sql
+++ CS50SQL/pset1/packages/log.sql
@@ -4,6 +4,8 @@
 -- Find the addresses in the address table
 sqlite> SELECT * FROM addresses WHERE address = "900 Somerville Avenue";
 
+sqlite> SELECT * FROM addresses WHERE address = "2 Finnegan Street"; -- this query yields no match
+
 
 
 -- *** The Devious Delivery ***
