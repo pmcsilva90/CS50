ef004e0fcd721c90d60e1483c93606e7023eba51
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 14:14:14 +0000

    Sat, Apr 6, 2024, 3:14 PM +01:00

--- CS50SQL/pset1/packages/log.sql
+++ CS50SQL/pset1/packages/log.sql
@@ -1,6 +1,9 @@
 
 -- *** The Lost Letter ***
 
+-- Find the addresses in the address table
+sqlite> SELECT * FROM addresses WHERE address = "900 Somerville Avenue";
+
 
 
 -- *** The Devious Delivery ***
