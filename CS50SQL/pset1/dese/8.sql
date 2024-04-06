b109f0e89e56a0e8c6d23fb2a0622cd17d5a2881
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 19:12:10 +0000

    Sat, Apr 6, 2024, 8:12 PM +01:00

--- CS50SQL/pset1/dese/8.sql
+++ CS50SQL/pset1/dese/8.sql
@@ -1,1 +1,2 @@
-SELECT name, pupils FROM districts JOIN expenditures ON districts.id = expenditures.district_id;
+SELECT name, pupils FROM districts
+JOIN expenditures ON districts.id = expenditures.district_id;
