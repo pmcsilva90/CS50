9dec8c23c66819a8f3df38254ba7eaddd977b6d3
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 14:29:22 +0000

    Sat, Apr 6, 2024, 3:29 PM +01:00

--- CS50SQL/pset1/packages/log.sql
+++ CS50SQL/pset1/packages/log.sql
@@ -1,21 +1,22 @@
 
 -- *** The Lost Letter ***
 
-sqlite> SELECT * FROM addresses WHERE address = "2 Finnegan Street"; -- this query yields no match
+-- Find the receiver's address in the addresses table
+SELECT * FROM addresses WHERE address = "2 Finnegan Street"; -- this query yields no match
 
 
 -- Find the sender's address in the address table
-sqlite> SELECT * FROM addresses WHERE address = "900 Somerville Avenue";
+SELECT * FROM addresses WHERE address = "900 Somerville Avenue";
 
 -- Find packages sent from sender's address
-sqlite> SELECT * FROM packages WHERE from_address_id = (
-   ...> SELECT id FROM addresses WHERE address = "900 Somerville Avenue");
+SELECT * FROM packages WHERE from_address_id = (
+SELECT id FROM addresses WHERE address = "900 Somerville Avenue");
 
 -- Given one of the packges contents has the description "Congratulatory letter", it can be assumed to be the lost letter. The query shows it was sent to address ID 854
 
 -- Find the address with ID of 854
-sqlite> SELECT * FROM addresses WHERE id = 854;
--- query returns address 2 Finnigan Street, which is one character different from Anneke's spelling.
+SELECT * FROM addresses WHERE id = 854;
+-- Query returns address 2 Finnigan Street, which is one character different from Anneke's spelling.
 
 
 
