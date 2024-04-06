9d0420898535dc62ce6b6c875e5a930ddeddc27f
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 14:16:43 +0000

    Sat, Apr 6, 2024, 3:16 PM +01:00

--- CS50SQL/pset1/packages/log.sql
+++ CS50SQL/pset1/packages/log.sql
@@ -1,10 +1,14 @@
 
 -- *** The Lost Letter ***
 
--- Find the addresses in the address table
+sqlite> SELECT * FROM addresses WHERE address = "2 Finnegan Street"; -- this query yields no match
+
+
+-- Find the sender's address in the address table
 sqlite> SELECT * FROM addresses WHERE address = "900 Somerville Avenue";
 
-sqlite> SELECT * FROM addresses WHERE address = "2 Finnegan Street"; -- this query yields no match
+
+
 
 
 
