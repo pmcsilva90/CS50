b29eb1052c24a66f471ca7663e3df39d3fcad5e4
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 19:34:33 +0000

    Sat, Apr 6, 2024, 8:34 PM +01:00

--- CS50SQL/pset1/dese/11.sql
+++ CS50SQL/pset1/dese/11.sql
@@ -1,12 +1,12 @@
 SELECT
-    name,
-    per_pupil_expenditure,
-    graduated
+    schools.name,
+    expenditures.per_pupil_expenditure,
+    graduation_rates.graduated
 FROM
     schools
     JOIN graduation_rates ON schools.id = graduation_rates.school_id
     JOIN districts ON schools.district_id = districts.id
     JOIN expenditures ON districts.id = expenditures.district_id
 ORDER BY
-    per_pupil_expenditure DESC,
-    name;
+    expenditures.per_pupil_expenditure DESC,
+    schools.name;
