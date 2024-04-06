30735f1157b10a204bbf543658586196f99c45b1
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-06 14:20:06 +0000

    Sat, Apr 6, 2024, 3:20 PM +01:00

--- CS50SQL/pset1/packages/log.sql
+++ CS50SQL/pset1/packages/log.sql
@@ -7,6 +7,10 @@ sqlite> SELECT * FROM addresses WHERE address = "2 Finnegan Street"; -- this que
 -- Find the sender's address in the address table
 sqlite> SELECT * FROM addresses WHERE address = "900 Somerville Avenue";
 
+-- Find packages sent from sender's address
+sqlite> SELECT * FROM packages WHERE from_address_id = (
+   ...> SELECT id FROM addresses WHERE address = "900 Somerville Avenue");
+
 
 
 
