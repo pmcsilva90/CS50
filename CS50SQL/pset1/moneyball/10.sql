0b414f5a0788106b25e3ff46fa6ae1c5d11cec35
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 14:14:12 +0000

    Sun, Apr 7, 2024, 3:14 PM +01:00

--- CS50SQL/pset1/moneyball/10.sql
+++ CS50SQL/pset1/moneyball/10.sql
@@ -1,10 +1,10 @@
--- sqlite> select *, sum(performances.HR) from performances group by player_id order by player_id limit 100;
 SELECT
     players.first_name,
     players.last_name,
     salaries.salary,
     salaries.year AS "salary year",
-    performances.year AS "performance year"
+    performances.year AS "performance year",
+    performances."home runs"
 FROM
     players
     JOIN salaries ON players.id = salaries.player_id
