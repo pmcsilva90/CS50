998590ca6835016def80f7cf1bcb824c8962b852
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 15:30:27 +0000

    Sun, Apr 7, 2024, 4:30 PM +01:00

--- CS50SQL/pset1/moneyball/11.sql
+++ CS50SQL/pset1/moneyball/11.sql
@@ -1,11 +1,28 @@
 SELECT
-    *
-FROM
-    salaries
-    JOIN performances ON salaries.player_id = performances.player_id
-ORDER BY
-    player_id,
+    players.first_name,
+    players.last_name,
+    salaries.salary,
     salaries.year,
-    performances.year
-LIMIT
-    100;
+    performances.year,
+    performances."home runs"
+FROM
+    players
+    JOIN salaries ON players.id = salaries.player_id
+    JOIN (
+        SELECT
+            performancesances.player_id,
+            performancesances.year,
+            SUM(HR) AS "home runs"
+        FROM
+            performancesances
+        GROUP BY
+            performancesances.player_id,
+            performancesances.year
+    ) AS performances ON salaries.player_id = performances.player_id
+ORDER BY
+    players.id,
+    salaries.year DESC,
+    performances."home runs" DESC,
+    salaries.salary DESC;
+
+    -- Todd Zeile id = 20728
