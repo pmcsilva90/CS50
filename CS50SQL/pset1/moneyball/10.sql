bceb8cd3b6c39564b7ca301f08c92bda49420820
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 14:19:31 +0000

    Sun, Apr 7, 2024, 3:19 PM +01:00

--- CS50SQL/pset1/moneyball/10.sql
+++ CS50SQL/pset1/moneyball/10.sql
@@ -3,8 +3,7 @@ SELECT
     players.last_name,
     salaries.salary,
     performances."home runs",
-    salaries.year AS "salary year",
-    performances.year AS "performance year"
+    salaries.year
 FROM
     players
     JOIN salaries ON players.id = salaries.player_id
@@ -20,7 +19,4 @@ FROM
 ORDER BY
     players.first_name,
     players.last_name,
-    salaries.year,
-    performances.year
-LIMIT
-    100;
+    salaries.year;
