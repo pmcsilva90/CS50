d88c9ac4fbe9d7413d136e6ec6f7518106b26ea9
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 18:38:15 +0000

    Sun, Apr 7, 2024, 7:38 PM +01:00

---
+++ CS50SQL/pset2/atl/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE passengers
+    (
+        id INTEGER,
+        first_name TEXT,
+        last_name TEXT,
+        age INTEGER
+        PRIMARY KEY(id)
+    )
+
+CREATE TABLE check_ins(
+id integer
+
+)
