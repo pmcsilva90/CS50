d93d61cd38566150b1cacd82a59414d517d4d2d0
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 13:34:10 +0000

    Sun, Apr 7, 2024, 2:34 PM +01:00

--- CS50SQL/pset1/moneyball/10.sql
+++ CS50SQL/pset1/moneyball/10.sql
@@ -9,7 +9,7 @@ FROM
     JOIN salaries ON players.id = salaries.player_id
     JOIN performances ON players.id = performances.player_id
 
-    group by players.id;
+    group by performances.year;
 
 
 select sum(performances.HR) from performances group by player_id
