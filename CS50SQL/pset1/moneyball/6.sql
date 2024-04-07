4c6da028654973317e74c9d3d865cc16eeba693f
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 12:08:53 +0000

    Sun, Apr 7, 2024, 1:08 PM +01:00

--- CS50SQL/pset1/moneyball/6.sql
+++ CS50SQL/pset1/moneyball/6.sql
@@ -1,1 +1,14 @@
-SELECT teams.name, performances.H 
+SELECT
+    teams.name,
+    SUM(H) AS "total hits"
+FROM
+    teams
+    JOIN performances
+WHERE
+    teams.id = performances.team_id
+WHERE
+    YEAR = 2001
+GROUP BY
+    performances.team_id
+ORDER BY
+    "total hits";
