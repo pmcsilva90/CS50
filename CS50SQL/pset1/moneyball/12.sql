2298239b9944a0d543f40010b5e9deccfa4b6ec0
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 18:02:59 +0000

    Sun, Apr 7, 2024, 7:02 PM +01:00

--- CS50SQL/pset1/moneyball/12.sql
+++ CS50SQL/pset1/moneyball/12.sql
@@ -1,3 +1,26 @@
+
+
+SELECT
+    players.first_name,
+    players.last_name,
+    (salaries.salary / performances.h) AS "dollars per hit"
+FROM
+    players
+    JOIN performances ON players.id = performances.player_id
+    JOIN salaries ON performances.player_id = salaries.player_id
+    AND performances.year = salaries.year
+WHERE
+    performances.h > 0
+    AND salaries.year = 2001
+ORDER BY
+    "dollars per hit",
+    players.first_name,
+    players.last_name
+LIMIT
+    10;
+
+
+
 SELECT
     players.first_name,
     players.last_name,
