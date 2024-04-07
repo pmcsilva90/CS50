e4b5598ad3fe883791ddef8215344d658b0156de
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 13:28:04 +0000

    Sun, Apr 7, 2024, 2:28 PM +01:00

--- CS50SQL/pset1/moneyball/10.sql
+++ CS50SQL/pset1/moneyball/10.sql
@@ -3,4 +3,5 @@ select players.first_name, players.last_name, salaries.salary, performances.HR, 
 players join salaries on players.id = salaries.player_id
 join performances on players.id = performances.player_id
 
-where 
+
+
