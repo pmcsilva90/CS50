8071119e59fec9ab1d6b9b38b84447197c67a36e
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 18:00:45 +0000

    Sun, Apr 7, 2024, 7:00 PM +01:00

---
+++ CS50SQL/pset1/moneyball/12.sql
@@ -0,0 +1,18 @@
+SELECT
+    players.first_name,
+    players.last_name,
+    (salaries.salary / performances.RBI) AS "dollars per RBI"
+FROM
+    players
+    JOIN performances ON players.id = performances.player_id
+    JOIN salaries ON performances.player_id = salaries.player_id
+    AND performances.year = salaries.year
+WHERE
+    performances.RBI > 0
+    AND salaries.year = 2001
+ORDER BY
+    "dollars per RBI",
+    players.first_name,
+    players.last_name
+LIMIT
+    10;
