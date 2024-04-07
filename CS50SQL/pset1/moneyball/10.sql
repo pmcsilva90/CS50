055ff0e0cb4ef4781d915d3fa3f70528c0c9fe0f
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 14:02:01 +0000

    Sun, Apr 7, 2024, 3:02 PM +01:00

--- CS50SQL/pset1/moneyball/10.sql
+++ CS50SQL/pset1/moneyball/10.sql
@@ -3,12 +3,13 @@ SELECT
     players.first_name,
     players.last_name,
     salaries.salary,
-    salaries.year
+    salaries.year as "salary year",
+    performances.year as "performance year"
 FROM
     players
     JOIN salaries ON players.id = salaries.player_id
     JOIN performances ON salaries.player_id = performances.player_id
 ORDER BY
-    players.id
+    players.id, salaries.year
 LIMIT
     100;
