25dcacacefdaeee36c94d372bc903785450f7c0b
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 16:05:53 +0000

    Sun, Apr 7, 2024, 5:05 PM +01:00

--- CS50SQL/pset1/moneyball/11.sql
+++ CS50SQL/pset1/moneyball/11.sql
@@ -1,5 +1,34 @@
-Select players.first_name, players.last_name, salaries.salary, performances.hr from
+--Select players.first_name, players.last_name, salaries.salary, performances.hr from
 
-select * from
-players join salaries on player.id = salaries.player_id
+--select * from
+--players join salaries on player.id = salaries.player_id
+
+SELECT
+    players.first_name,
+    players.last_name,
+    salaries.salary,
+    salaries.year,
+    perform.year,
+    performances.HR
+FROM
+    players
+    JOIN salaries ON players.id = salaries.player_id
+    JOIN (
+        SELECT
+            performances.player_id,
+            performances.year,
+            performances.HR
+        FROM
+            performances
+        GROUP BY
+            performances.player_id,
+            performances.year
+    ) AS perform ON salaries.player_id = perform.player_id
+ORDER BY
+    players.id,
+    salaries.year DESC,
+    performances.HR DESC,
+    salaries.salary DESC;
+
+    -- Todd Zeile id = 20728
 
