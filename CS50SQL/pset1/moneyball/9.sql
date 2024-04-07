7a708265db3bb97c8147532e3131fc3dff3ee9bd
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 13:21:46 +0000

    Sun, Apr 7, 2024, 2:21 PM +01:00

--- CS50SQL/pset1/moneyball/9.sql
+++ CS50SQL/pset1/moneyball/9.sql
@@ -1,1 +1,14 @@
-select avg(salary) as "average salary" from salaries group by team_id order by "average salary" limit 10;
+SELECT
+    teams.name,
+    ROUND(AVG(salary), 2) AS "average salary"
+FROM
+    salaries
+    JOIN teams ON salaries.team_id = teams.id
+WHERE
+    salaries.year = 2001
+GROUP BY
+    teams.name
+ORDER BY
+    "average salary"
+LIMIT
+    10;
