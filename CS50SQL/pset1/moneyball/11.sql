1e8f437016d23a27cb757c938cf1ac1b991fb7ce
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 16:53:39 +0000

    Sun, Apr 7, 2024, 5:53 PM +01:00

--- CS50SQL/pset1/moneyball/11.sql
+++ CS50SQL/pset1/moneyball/11.sql
@@ -1,1 +1,11 @@
-select 
+SELECT
+    --performances.h,
+    --salaries.salary
+    *
+FROM
+    performances
+    JOIN salaries ON performances.player_id = salaries.player_id
+    AND performances.year = salaries.year
+    order by player_id
+LIMIT
+    100;
