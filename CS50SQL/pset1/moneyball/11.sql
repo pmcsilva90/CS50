b931971ef5105ae6a1d866aed0bf60edabf4e215
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 16:00:48 +0000

    Sun, Apr 7, 2024, 5:00 PM +01:00

--- CS50SQL/pset1/moneyball/11.sql
+++ CS50SQL/pset1/moneyball/11.sql
@@ -1,1 +1,5 @@
-SELECT player_id from performances union SELECT player_id from salaries;
+Select players.first_name, players.last_name, salaries.salary, performances.hr from
+
+select * from
+players join salaries on player.id = salaries.player_id
+
