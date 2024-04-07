a1fb1d7e8a68ac901fc4612016a3130db03bf852
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 18:08:53 +0000

    Sun, Apr 7, 2024, 7:08 PM +01:00

--- CS50SQL/pset1/moneyball/12.sql
+++ CS50SQL/pset1/moneyball/12.sql
@@ -1,44 +1,49 @@
-SELECT first_name, last_name from
-(
 SELECT
-    players.first_name,
-    players.last_name,
-    (salaries.salary / performances.h) AS "dollars per hit"
+    first_name,
+    last_name
 FROM
-    players
-    JOIN performances ON players.id = performances.player_id
-    JOIN salaries ON performances.player_id = salaries.player_id
-    AND performances.year = salaries.year
-WHERE
-    performances.h > 0
-    AND salaries.year = 2001
-ORDER BY
-    "dollars per hit",
-    players.first_name,
-    players.last_name
-LIMIT
-    10
-)
+    (
+        SELECT
+            players.first_name,
+            players.last_name,
+            (salaries.salary / performances.h) AS "dollars per hit"
+        FROM
+            players
+            JOIN performances ON players.id = performances.player_id
+            JOIN salaries ON performances.player_id = salaries.player_id
+            AND performances.year = salaries.year
+        WHERE
+            performances.h > 0
+            AND salaries.year = 2001
+        ORDER BY
+            "dollars per hit",
+            players.first_name,
+            players.last_name
+        LIMIT
+            10
+    )
 INTERSECT
-SELECT first_name, last_name FROM
-(
 SELECT
-    players.first_name,
-    players.last_name,
-    (salaries.salary / performances.RBI) AS "dollars per RBI"
+    first_name,
+    last_name
 FROM
-    players
-    JOIN performances ON players.id = performances.player_id
-    JOIN salaries ON performances.player_id = salaries.player_id
-    AND performances.year = salaries.year
-WHERE
-    performances.RBI > 0
-    AND salaries.year = 2001
-ORDER BY
-    "dollars per RBI",
-    players.first_name,
-    players.last_name
-LIMIT
-    10
-)
-;
+    (
+        SELECT
+            players.first_name,
+            players.last_name,
+            (salaries.salary / performances.RBI) AS "dollars per RBI"
+        FROM
+            players
+            JOIN performances ON players.id = performances.player_id
+            JOIN salaries ON performances.player_id = salaries.player_id
+            AND performances.year = salaries.year
+        WHERE
+            performances.RBI > 0
+            AND salaries.year = 2001
+        ORDER BY
+            "dollars per RBI",
+            players.first_name,
+            players.last_name
+        LIMIT
+            10
+    );
