b7dcc8d12600edd644179f08c3827246630401a6
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 12:26:18 +0000

    Sun, Apr 7, 2024, 1:26 PM +01:00

--- CS50SQL/pset1/moneyball/8.sql
+++ CS50SQL/pset1/moneyball/8.sql
@@ -1,5 +1,21 @@
-
-
-
-
-select * from performances where 
+SELECT
+    *
+FROM
+    salaries
+WHERE
+    salaries.player_id = (
+        SELECT
+            performances.player_id
+        FROM
+            performances
+        WHERE
+            performances.HR = (
+                SELECT
+                    MAX(performances.HR)
+                FROM
+                    performances
+                WHERE
+                    performances.year = 2001
+            )
+    )
+    AND salaries.year = 2001;
