ee61c3f148e880023e56c0bd6f9cc22ddbd40bad
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-07 18:50:58 +0000

    Sun, Apr 7, 2024, 7:50 PM +01:00

--- CS50SQL/pset2/atl/schema.sql
+++ CS50SQL/pset2/atl/schema.sql
@@ -1,13 +1,28 @@
-CREATE TABLE passengers
-    (
-        id INTEGER,
-        first_name TEXT,
-        last_name TEXT,
-        age INTEGER
-        PRIMARY KEY(id)
+CREATE TABLE
+    passengers (
+        "id" INTEGER,
+        "first_name" TEXT,
+        "last_name" TEXT,
+        "age" INTEGER,
+        PRIMARY KEY ("id")
+    );
+
+CREATE TABLE
+    check_ins (
+        "id" INTEGER,
+        "timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "passenger_id" INTEGER,
+        "flight_id" INTEGER,
+        PRIMARY key ("id"),
+        FOREIGN key ("passenger_id") REFERENCES "passengers" ("id"),
+        FOREIGN key ("flight_id") REFERENCES "flights" ("flight_id")
+    );
+
+CREATE TABLE
+    airlines (
+        "id" INTEGER,
+        "concourse" text NOT NULL CHECK (
+            "concourse" IN ('A', 'B', 'C', 'D', 'E', 'F', 'T')
+        ),
+        PRIMARY key "id"
     )
-
-CREATE TABLE check_ins(
-id integer
-
-)
