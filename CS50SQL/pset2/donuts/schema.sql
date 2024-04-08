8b9de662eda5a3f9eb49c851d15d17784fd5365d
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-08 13:04:36 +0000

    Mon, Apr 8, 2024, 2:04 PM +01:00

---
+++ CS50SQL/pset2/donuts/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE
+    ingredients (
+        id INTEGER,
+        name TEXT NOT NULL UNIQUE,
+        unit TEXT NOT NULL,
+        unit_qty INTEGER NOT NULL,
+        price_per_unit NUMERIC NOT NULL
+        PRIMARY KEY (id)
+    );
+
+CREATE TABLE donuts (
+    id INTEGER
+    name TEXT NOT NULL
+    gluten_free TEXT NOT NULL CHECK(gluten_free IN ('yes', 'no'))
+    price_per_donut NUMERIC NOT NULL
+)
