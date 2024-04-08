27c9c24fa7533b7e41e17f43bc025c9aa2a5e5e8
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-08 13:05:30 +0000

    Mon, Apr 8, 2024, 2:05 PM +01:00

--- CS50SQL/pset2/donuts/schema.sql
+++ CS50SQL/pset2/donuts/schema.sql
@@ -9,8 +9,9 @@ CREATE TABLE
     );
 
 CREATE TABLE donuts (
-    id INTEGER
-    name TEXT NOT NULL
-    gluten_free TEXT NOT NULL CHECK(gluten_free IN ('yes', 'no'))
-    price_per_donut NUMERIC NOT NULL
+    id INTEGER,
+    name TEXT NOT NULL,
+    gluten_free TEXT NOT NULL CHECK(gluten_free IN ('yes', 'no')),
+    price_per_donut NUMERIC NOT NULL,
+    PRIMARY KEY (id)
 )
