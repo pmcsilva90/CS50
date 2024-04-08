66967040d57e1736355f0e0c8878adf59586df13
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-08 14:36:56 +0000

    Mon, Apr 8, 2024, 3:36 PM +01:00

--- CS50SQL/pset2/donuts/schema.sql
+++ CS50SQL/pset2/donuts/schema.sql
@@ -39,14 +39,14 @@ CREATE TABLE
 
 CREATE TABLE
     order_history (
+        order_timestamp NUMERIC,
         order_id INTEGER,
         customer_id INTEGER,
         order_contents INTEGER,
-        order_timestamp NUMERIC,
+        FOREIGN KEY (order_timestamp) REFERENCES orders (timestamp),
         FOREIGN KEY (order_id) REFERENCES orders (id),
         FOREIGN KEY (customer_id) REFERENCES customers (id),
-        FOREIGN KEY (order_contents) REFERENCES donuts (id),
-        FOREIGN KEY (order_timestamp) REFERENCES orders (timestamp)
+        FOREIGN KEY (order_contents) REFERENCES donuts (id)
     );
 
 CREATE TABLE
