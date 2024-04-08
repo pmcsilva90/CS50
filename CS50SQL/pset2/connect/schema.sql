3c768e44836de9a61e161e7a577e35b56cef9ee5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-08 10:57:24 +0000

    Mon, Apr 8, 2024, 11:57 AM +01:00

--- CS50SQL/pset2/connect/schema.sql
+++ CS50SQL/pset2/connect/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE
     users (
         id INTEGER,
         username text NOT NULL UNIQUE,
-        password text NOT NULL
+        password text NOT NULL check (length(password >= 8))
     )
