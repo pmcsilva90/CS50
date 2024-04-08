838021177d5dfbaf97a02815ac8a6618d735fd80
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-08 12:29:03 +0000

    Mon, Apr 8, 2024, 1:29 PM +01:00

--- CS50SQL/pset2/connect/schema.sql
+++ CS50SQL/pset2/connect/schema.sql
@@ -7,24 +7,48 @@ CREATE TABLE
         password text NOT NULL CHECK (length (password >= 8)),
     );
 
-create table people_connections(
-    user_id1 integer foreign key references users(id),
-    user_id2 integer foreign key references users(id),
-
-
-)
-
 CREATE TABLE
-    institutions (
+    education_institutions (
         id INTEGER PRIMARY key,
         type text NOT NULL,
         location text,
         founded INTEGER
-    )
+    );
+
 CREATE TABLE
     companies (
         id INTEGER PRIMARY key,
         name text UNIQUE NOT NULL,
         industry text NOT NULL,
         location text NOT NULL
-    )
+    );
+
+CREATE TABLE
+    users_connections (
+        user1_id INTEGER,
+        user2_id INTEGER,
+        FOREIGN key (user1_id) REFERENCES users (id),
+        FOREIGN key (user2_id) REFERENCES users (id)
+    );
+
+CREATE TABLE
+    users_education (
+        user_id INTEGER,
+        institution_id INTEGER,
+        start_date DATE,
+        end_date DATE,
+        degree text,
+        FOREIGN key user_id REFERENCES users (id),
+        FOREIGN key institution_id REFERENCES education_institutions (id)
+    );
+
+CREATE TABLE
+    users_employment (
+        user_id INTEGER,
+        company_id INTEGER,
+        stard_date DATE,
+        end_date DATE,
+        role text,
+        FOREIGN key user_id REFERENCES users (id),
+        FOREIGN key company_id REFERENCES companies (id)
+    );
