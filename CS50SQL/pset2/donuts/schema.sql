c98e98d98805562e0077e9484b8a1ada0db3c1de
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-08 13:12:44 +0000

    Mon, Apr 8, 2024, 2:12 PM +01:00

--- CS50SQL/pset2/donuts/schema.sql
+++ CS50SQL/pset2/donuts/schema.sql
@@ -11,15 +11,17 @@ CREATE TABLE
 CREATE TABLE donuts (
     id INTEGER,
     name TEXT NOT NULL,
-    gluten_free TEXT NOT NULL CHECK(gluten_free IN ('yes', 'no')),
+    gluten_free TEXT NOT NULL CHECK(gluten_free IN ('Yes', 'No')),
     price_per_donut NUMERIC NOT NULL,
     PRIMARY KEY (id)
 )
 
 CREATE TABLE orders (
-    id
-    instore_online
-    customer_name
-    
-
+    id INTEGER,
+    instore_online TEXT NOT NULL CHECK(instore_online IN ('In store', 'Online')),
+    customer_id INTEGER,
+    PRIMARY KEY (id),
+    FOREGIN KEY (customer_id) REFERENCES customers(id)
 )
+
+
