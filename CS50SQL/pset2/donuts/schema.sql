e0729050a8322838a81d71621790e9910838a223
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-08 13:32:38 +0000

    Mon, Apr 8, 2024, 2:32 PM +01:00

--- CS50SQL/pset2/donuts/schema.sql
+++ CS50SQL/pset2/donuts/schema.sql
@@ -4,24 +4,31 @@ CREATE TABLE
         name TEXT NOT NULL UNIQUE,
         unit TEXT NOT NULL,
         unit_qty INTEGER NOT NULL,
-        price_per_unit NUMERIC NOT NULL
-        PRIMARY KEY (id)
+        price_per_unit NUMERIC NOT NULL PRIMARY KEY (id)
     );
 
-CREATE TABLE donuts (
-    id INTEGER,
-    name TEXT NOT NULL,
-    gluten_free TEXT NOT NULL CHECK(gluten_free IN ('Yes', 'No')),
-    price_per_donut NUMERIC NOT NULL,
-    PRIMARY KEY (id)
-)
-
-CREATE TABLE orders (
-    id INTEGER,
-    instore_online TEXT NOT NULL CHECK(instore_online IN ('In store', 'Online')),
-    customer_id INTEGER,
-    PRIMARY KEY (id),
-    FOREGIN KEY (customer_id) REFERENCES customers(id)
-)
-
-
+CREATE TABLE
+    donuts (
+        id INTEGER,
+        name TEXT NOT NULL,
+        gluten_free TEXT NOT NULL CHECK (gluten_free IN ('Yes', 'No')),
+        price_per_donut NUMERIC NOT NULL,
+        PRIMARY KEY (id)
+    );
+CREATE TABLE
+    orders (
+        id INTEGER,
+        timestamp NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        instore_online TEXT NOT NULL CHECK (instore_online IN ('In store', 'Online')),
+        customer_id INTEGER,
+        cost 
+        PRIMARY KEY (id),
+        FOREGIN KEY (customer_id) REFERENCES customers (id)
+    );
+CREATE TABLE
+    customers (
+        id INTEGER,
+        first_name TEXT,
+        last_name TEXT,
+        PRIMARY KEY (id)
+    );
