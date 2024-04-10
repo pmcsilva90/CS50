391d8cae24bb205cf877dcf0d705c02e9a374d12
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-10 15:20:34 +0000

    Wed, Apr 10, 2024, 4:20 PM +01:00

--- CS50SQL/pset3/dont-panic/hack.sql
+++ CS50SQL/pset3/dont-panic/hack.sql
@@ -9,24 +9,33 @@ WHERE
     new_password = '982c0381c279d139fd221fce974916e7';
 
 -- Add false data to throw others off your trail. In particular, to frame emily33, make it only appear—in the user_logs table—as if the admin account has had its password changed to emily33’s password.
-
-
-(
-    'update',
-    'admin',
-    'admin',
+INSERT INTO
+    user_logs (
+        type,
+        old_username,
+        new_username,
+        old_password,
+        new_password
+    )
+VALUES
     (
-        SELECT
-            password
-        FROM
-            users
-        WHERE
-            username = 'admin'
-    ),
-    SELECT
-        password
-    FROM
-        users
-    WHERE
-        username = 'emily33'
-)
+        'update',
+        'admin',
+        'admin',
+        (
+            SELECT
+                password
+            FROM
+                users
+            WHERE
+                username = 'admin'
+        ),
+        (
+            SELECT
+                password
+            FROM
+                users
+            WHERE
+                username = 'emily33'
+        )
+    );
