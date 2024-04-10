547767ae7d14bb9c81d81e0a0dbd79709e23f382
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-10 16:32:49 +0000

    Wed, Apr 10, 2024, 5:32 PM +01:00

--- CS50SQL/pset3/meteorites/import.sql
+++ CS50SQL/pset3/meteorites/import.sql
@@ -6,64 +6,62 @@
 --     year, which is the year in which the the meteorite was discovered.
 --     lat, which is the latitude at which the meteorite landed.
 --     long, which is the longitude at which the meteorite landed.
-
 /* CREATE TABLE
-    "meteorites" (
-        "id" INTEGER,
-        "name" TEXT NOT NULL UNIQUE,
-        "class" TEXT,
-        "mass" REAL,
-        "discovery" TEXT CHECK ("discovery" IN ('Found', 'Fell')),
-        "year" INTEGER,
-        "lat" REAL,
-        "long" REAL,
-        PRIMARY KEY ("id")
-    ); */
-
+"meteorites" (
+"id" INTEGER,
+"name" TEXT NOT NULL UNIQUE,
+"class" TEXT,
+"mass" REAL,
+"discovery" TEXT CHECK ("discovery" IN ('Found', 'Fell')),
+"year" INTEGER,
+"lat" REAL,
+"long" REAL,
+PRIMARY KEY ("id")
+); */
 -- Keep in mind that not all columns in the CSV should end up in the final table!
 -- To consider the data in the meteorites table clean, you should ensure…
 --     Any empty values in meteorites.csv are represented by NULL in the meteorites table.
 --         Keep in mind that the mass, year, lat, and long columns have empty values in the CSV.
-
 /* UPDATE temp
 SET
-    mass = NULL
+mass = NULL
 WHERE
-    mass = '';
+mass = '';
 
 UPDATE temp
 SET
-    YEAR = NULL
+YEAR = NULL
 WHERE
-    YEAR = '';
+YEAR = '';
 
 UPDATE temp
 SET
-    lat = NULL
+lat = NULL
 WHERE
-    lat = '';
+lat = '';
 
 UPDATE temp
 SET
-    long = NULL
+long = NULL
 WHERE
-    long = ''; */
-
+long = ''; */
 --     All columns with decimal values (e.g., 70.4777) should be rounded to the nearest hundredths place (e.g., 70.4777 becomes 70.48).
 --         Keep in mind that the mass, lat, and long columns have decimal values.
 UPDATE temp
 SET
-    mass = round(mass, 2);
+    mass = ROUND(mass, 2);
 
 UPDATE temp
 SET
-    lat = round(lat, 2);
+    lat = ROUND(lat, 2);
 
 UPDATE temp
 SET
-    long = round(long, 2);
+    long = ROUND(long, 2);
 
 --     All meteorites with the nametype “Relict” are not included in the meteorites table.
+
+
 --     The meteorites are sorted by year, oldest to newest, and then—if any two meteorites landed in the same year—by name, in alphabetical order.
 --     You’ve updated the IDs of the meteorites from meteorites.csv, according to the order specified in #4.
 --         The id of the meteorites should start at 1, beginning with the meteorite that landed in the oldest year and is the first in alphabetical order for that year.
