1bc76b61e433933ae132ffacb0f2b782c8331195
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-10 18:03:41 +0000

    Wed, Apr 10, 2024, 7:03 PM +01:00

--- CS50SQL/pset3/meteorites/import.sql
+++ CS50SQL/pset3/meteorites/import.sql
@@ -1,14 +1,4 @@
---     id, which represents the unique ID of the meteorite.
---     name, which represents the given name of the meteorite.
---     class, which is the classification of the meteorite, according to the traditional classification scheme.
---     mass, which is the weight of the meteorite, in grams.
---     discovery, which is either “Fell” or “Found”. “Fell” indicates the meteorite was seen falling to Earth, whereas “Found” indicates the meteorite was found only after landing on Earth.
---     year, which is the year in which the the meteorite was discovered.
---     lat, which is the latitude at which the meteorite landed.
---     long, which is the longitude at which the meteorite landed.
-
 .import -csv meteorites.csv temp
-
 
 CREATE TABLE
     "meteorites" (
@@ -23,10 +13,8 @@ CREATE TABLE
         PRIMARY KEY ("id")
     );
 
--- Keep in mind that not all columns in the CSV should end up in the final table!
--- To consider the data in the meteorites table clean, you should ensure…
---     Any empty values in meteorites.csv are represented by NULL in the meteorites table.
---         Keep in mind that the mass, year, lat, and long columns have empty values in the CSV.
+-- empty values in meteorites.csv are represented by NULL in the meteorites table.
+-- mass, year, lat, and long columns have empty values in the CSV.
 UPDATE temp
 SET
     mass = NULL
@@ -51,8 +39,8 @@ SET
 WHERE
     long = '';
 
---     All columns with decimal values (e.g., 70.4777) should be rounded to the nearest hundredths place (e.g., 70.4777 becomes 70.48).
---         Keep in mind that the mass, lat, and long columns have decimal values.
+-- All columns with decimal values (e.g., 70.4777) should be rounded to the nearest hundredths place (e.g., 70.4777 becomes 70.48).
+-- mass, lat, and long columns have decimal values.
 UPDATE temp
 SET
     mass = ROUND(mass, 2);
@@ -65,13 +53,12 @@ UPDATE temp
 SET
     long = ROUND(long, 2);
 
---     All meteorites with the nametype “Relict” are not included in the meteorites table.
+-- All meteorites with the nametype “Relict” are not included in the meteorites table.
 DELETE FROM temp
 WHERE
     nametype = 'Relict';
 
---     The meteorites are sorted by year, oldest to newest, and then—if any two meteorites landed in the same year—by name, in alphabetical order.
---  TODO: just copy the data to the created table
+-- The meteorites are sorted by year, oldest to newest, and then—if any two meteorites landed in the same year—by name, in alphabetical order.
 INSERT INTO
     meteorites (name, class, mass, discovery, year, lat, long)
 SELECT
@@ -89,5 +76,3 @@ ORDER BY
     name;
 
 DROP TABLE temp;
---     You’ve updated the IDs of the meteorites from meteorites.csv, according to the order specified in #4.
---         The id of the meteorites should start at 1, beginning with the meteorite that landed in the oldest year and is the first in alphabetical order for that year.
