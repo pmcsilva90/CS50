a9d9a025cd824370bcdeb32668cd055483c509c7
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-10 15:13:04 +0000

    Wed, Apr 10, 2024, 4:13 PM +01:00

--- CS50SQL/pset3/dont-panic/hack.sql
+++ CS50SQL/pset3/dont-panic/hack.sql
@@ -7,3 +7,7 @@ WHERE
 DELETE FROM user_logs
 WHERE
     new_password = '982c0381c279d139fd221fce974916e7'
+
+-- Add false data to throw others off your trail. In particular, to frame emily33, make it only appear—in the user_logs table—as if the admin account has had its password changed to emily33’s password.
+
+('update', 'admin', 'admin', )
