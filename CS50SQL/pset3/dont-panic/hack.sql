7e71ef383b5a3946949d5c1bfc807ef594343207
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-10 15:17:08 +0000

    Wed, Apr 10, 2024, 4:17 PM +01:00

--- CS50SQL/pset3/dont-panic/hack.sql
+++ CS50SQL/pset3/dont-panic/hack.sql
@@ -6,8 +6,27 @@ WHERE
 
 DELETE FROM user_logs
 WHERE
-    new_password = '982c0381c279d139fd221fce974916e7'
-    -- Add false data to throw others off your trail. In particular, to frame emily33, make it only appear—in the user_logs table—as if the admin account has had its password changed to emily33’s password.
-    ('update', 'admin', 'admin', ())
+    new_password = '982c0381c279d139fd221fce974916e7';
 
-SELECT 
+-- Add false data to throw others off your trail. In particular, to frame emily33, make it only appear—in the user_logs table—as if the admin account has had its password changed to emily33’s password.
+
+
+(
+    'update',
+    'admin',
+    'admin',
+    (
+        SELECT
+            password
+        FROM
+            users
+        WHERE
+            username = 'admin'
+    ),
+    SELECT
+        password
+    FROM
+        users
+    WHERE
+        username = 'emily33'
+)
