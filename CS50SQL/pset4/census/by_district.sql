1230cff180d1fffefbf431b5eed45df02160dbe4
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 12:49:33 +0000

    Thu, Apr 11, 2024, 1:49 PM +01:00

--- CS50SQL/pset4/census/by_district.sql
+++ CS50SQL/pset4/census/by_district.sql
@@ -2,11 +2,11 @@ CREATE VIEW
     by_district AS
 SELECT
     district,
-    SUM(families),
-    SUM(households),
-    SUM(population),
-    SUM(male),
-    SUM(female)
+    SUM(families) AS families,
+    SUM(households) AS households,
+    SUM(population) AS population,
+    SUM(male) AS male,
+    SUM(female) AS female
 FROM
     census
 GROUP BY
