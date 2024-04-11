2a8c843b1ba7250b75b71840a4fd58638335b803
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 13:23:57 +0000

    Thu, Apr 11, 2024, 2:23 PM +01:00

--- CS50SQL/pset4/private/private.sql
+++ CS50SQL/pset4/private/private.sql
@@ -1,2 +1,15 @@
-create view message as
-
+CREATE VIEW
+    triplet1 AS
+SELECT
+    sentence
+FROM
+    sentences
+WHERE
+    id = 14
+    OR id = 114;
+    OR id = 618;
+    OR id = 630;
+    OR id = 932;
+    OR id = 2230;
+    OR id = 2346
+    OR id = 3041;
