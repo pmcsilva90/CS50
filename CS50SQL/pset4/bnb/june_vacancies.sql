350eeb6013163500b8f403b0fadfad60794c29ba
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 15:12:23 +0000

    Thu, Apr 11, 2024, 4:12 PM +01:00

--- CS50SQL/pset4/bnb/june_vacancies.sql
+++ CS50SQL/pset4/bnb/june_vacancies.sql
@@ -1,3 +1,10 @@
 create view june_vacancies as
 
-select listings.id, listings.property_type, listings.host_name "" as days_vacant
+select listings.id, listings.property_type, listings.host_name,
+count(*)
+as days_vacant
+from listings
+join availabilities on listings.id = avalabilities.listing_id
+
+
+group by listings.id
