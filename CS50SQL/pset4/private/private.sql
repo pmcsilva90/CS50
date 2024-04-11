07b98df31c61e9a0ccd6a50a8c851f47a21c4e37
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 13:43:44 +0000

    Thu, Apr 11, 2024, 2:43 PM +01:00

--- CS50SQL/pset4/private/private.sql
+++ CS50SQL/pset4/private/private.sql
@@ -5,7 +5,18 @@ CREATE TABLE
         message_len INTEGER NOT NULL
     );
 
-insert into triplets (sentence_num, char_num, message_len) values
+INSERT INTO
+    triplets (sentence_num, char_num, message_len)
+VALUES
+    (14, 98, 4)
+    (114, 3, 5)
+    (618, 72, 9)
+    (630, 7, 3)
+    (932, 12, 5)
+    (2230, 50, 7)
+    (2346, 44, 10)
+    (3041, 14, 5)
+
 
 CREATE VIEW
     triplet1 AS
