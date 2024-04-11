655836dfb246138a97dd2eaf040dcef0336120d8
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 15:16:47 +0000

    Thu, Apr 11, 2024, 4:16 PM +01:00

--- CS50SQL/pset4/bnb/june_vacancies.sql
+++ CS50SQL/pset4/bnb/june_vacancies.sql
@@ -9,4 +9,4 @@ join availabilities on listings.id = avalabilities.listing_id
 
 group by listings.id
 
-select listing_id, count(*) from availabilities where date between '2023-06-01' and '2023-06-30' and available = 'TRUE'
+select listing_id, count(*) from availabilities where date between '2023-06-01' and '2023-06-30' and available = 'TRUE' group by listing_id order by listing_id limit 100;
