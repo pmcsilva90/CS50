c20ff139e24e62f8a9a4a90e3788cf8bbd57cdf5
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 15:02:56 +0000

    Thu, Apr 11, 2024, 4:02 PM +01:00

--- CS50SQL/pset4/bnb/frequently_reviewed.sql
+++ CS50SQL/pset4/bnb/frequently_reviewed.sql
@@ -1,6 +1,6 @@
 create view frequently_reviewed as
 select listings.id, listings.property_type, listings.host_name,
 
-select listing_id, count(*) from reviews group by listing_id
-
+select listing_id, count(*) as reviews from reviews group by listing_id
+order by reviews DESC
 limit 100
