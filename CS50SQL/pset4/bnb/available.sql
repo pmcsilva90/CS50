f6fbfdecd7fabd752da6301938204d8c4d7acfaf
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 14:55:42 +0000

    Thu, Apr 11, 2024, 3:55 PM +01:00

--- CS50SQL/pset4/bnb/available.sql
+++ CS50SQL/pset4/bnb/available.sql
@@ -1,3 +1,4 @@
 create view available as
-select listings.id, property_type, host_name, date from listings
-join availabilities on listings.id = availabilities.listing_id;
+select listings.id, property_type, host_name, date, available from listings
+join availabilities on listings.id = availabilities.listing_id
+where availabilities.available = 'TRUE';
