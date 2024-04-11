31cb556f38d4cca59a68212982799310d4da6488
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 13:25:51 +0000

    Thu, Apr 11, 2024, 2:25 PM +01:00

--- CS50SQL/pset4/private/private.sql
+++ CS50SQL/pset4/private/private.sql
@@ -1,15 +1,8 @@
 CREATE VIEW
     triplet1 AS
 SELECT
-    sentence
+    *
 FROM
     sentences
 WHERE
-    id = 14
-    OR id = 114
-    OR id = 618
-    OR id = 630
-    OR id = 932
-    OR id = 2230
-    OR id = 2346
-    OR id = 3041;
+    id IN (14, 114, 618);
