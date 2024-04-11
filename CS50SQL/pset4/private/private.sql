f19bbc2fa5b04c5c8719b1cd84457648fbfe9b59
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 13:24:35 +0000

    Thu, Apr 11, 2024, 2:24 PM +01:00

--- CS50SQL/pset4/private/private.sql
+++ CS50SQL/pset4/private/private.sql
@@ -6,10 +6,10 @@ FROM
     sentences
 WHERE
     id = 14
-    OR id = 114;
-    OR id = 618;
-    OR id = 630;
-    OR id = 932;
-    OR id = 2230;
+    OR id = 114
+    OR id = 618
+    OR id = 630
+    OR id = 932
+    OR id = 2230
     OR id = 2346
     OR id = 3041;
