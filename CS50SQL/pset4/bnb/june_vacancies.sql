f07e97e9bb6c62afd1229a22783875b04b021e97
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-04-11 15:19:47 +0000

    Thu, Apr 11, 2024, 4:19 PM +01:00

--- CS50SQL/pset4/bnb/june_vacancies.sql
+++ CS50SQL/pset4/bnb/june_vacancies.sql
@@ -1,12 +1,8 @@
 create view june_vacancies as
 
-select listings.id, listings.property_type, listings.host_name,
-count(*)
-as days_vacant
+select listings.id, listings.property_type, listings.host_name, days_vacant
 from listings
-join availabilities on listings.id = avalabilities.listing_id
+join (select listing_id, count(*) as days_vacant from availabilities where date between '2023-06-01' and '2023-06-30' and available = 'TRUE' group by listing_id order by listing_id) as availabilities
+on listings.id = avalabilities.listing_id
 
 
-group by listings.id
-
-select listing_id, count(*) from availabilities where date between '2023-06-01' and '2023-06-30' and available = 'TRUE' group by listing_id order by listing_id limit 100;
