99769086ff97cf8778038c55d4cd4048c76d93e1
pmcsilva90 <125271574+pmcsilva90@users.noreply.github.com>
2024-06-25 14:07:07 +0000

    Tue, Jun 25, 2024, 3:07 PM +01:00

--- CS50x/pset9/birthdays/app.py
+++ CS50x/pset9/birthdays/app.py
@@ -34,34 +34,34 @@ def index():
         # Add the user's entry into the database
         name = request.form.get("name")
         if not name:
-            return render_template("index.html", birthdays=rows, months=monthserror_message="Error adding birthday: must include name")
+            return render_template("index.html", birthdays=rows, months=months, error_message="Error adding birthday: must include name")
             #return redirect("/")
 
         day = request.form.get("day")
         if not day:
-            return render_template("index.html", error_message="Error adding birthday: must include day")
+            return render_template("index.html", birthdays=rows, months=months, error_message="Error adding birthday: must include day")
             #return redirect("/")
         try:
             day = int(day)
         except ValueError:
-            return render_template("index.html", error_message="Error adding birthday: invalid day format")
+            return render_template("index.html", birthdays=rows, months=months, error_message="Error adding birthday: invalid day format")
             #return redirect("/")
         if day < 1 or day > 31:
-            return render_template("index.html", error_message="Error adding birthday: invalid day value")
+            return render_template("index.html", birthdays=rows, months=months, error_message="Error adding birthday: invalid day value")
             #return redirect("/")
 
 
         month = request.form.get("month")
         if not month:
-            return render_template("index.html", error_message="Error adding birthday: must include month")
+            return render_template("index.html", birthdays=rows, months=months, error_message="Error adding birthday: must include month")
             # return redirect("/")
         try:
             month = int(month)
         except ValueError:
-            return render_template("index.html", error_message="Error adding birthday: invalid month format")
+            return render_template("index.html", birthdays=rows, months=months, error_message="Error adding birthday: invalid month format")
             # return redirect("/")
         if month < 1 or month > 12:
-            return render_template("index.html", error_message="Error adding birthday: invalid month value")
+            return render_template("index.html", birthdays=rows, months=months, error_message="Error adding birthday: invalid month value")
             #return redirect("/")
 
         print(f"Name is {name}")
